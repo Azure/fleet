5f4032fa4046531365e44bfdba7564bd1a784e7d
Heba Elayoty <hebaelayoty@gmail.com>
2022-05-11 14:45:10 -0700

    Code review connets for api documentation

--- apis/v1alpha1/membercluster_types.go
+++ apis/v1alpha1/membercluster_types.go
@@ -11,14 +11,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// +enum
-type ClusterState string
-
-const (
-	ClusterStateJoin    ClusterState = "Join"
-	ClusterStateLeave   ClusterState = "Leave"
-	ClusterStateUpgrade ClusterState = "Upgrade"
-)
+// MemberCluster is a resource created in the hub cluster to represent a member cluster within a fleet.
 
 // +kubebuilder:object:root=true
 // +kubebuilder:resource:scope=Cluster,categories={fleet},shortName=cluster
@@ -26,7 +19,6 @@ const (
 // +kubebuilder:printcolumn:JSONPath=`.status.conditions[?(@.type=="ConditionTypeJoined")].status`,name="Joined",type=string
 // +kubebuilder:printcolumn:JSONPath=`.metadata.creationTimestamp`,name="Age",type=date
 // +kubebuilder:printcolumn:JSONPath=`.metadata.label[fleet.azure.com/clusterHealth]`,name="HealthStatus",type=string
-
 type MemberCluster struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -35,46 +27,49 @@ type MemberCluster struct {
 	Status MemberClusterStatus `json:"status,omitempty"`
 }
 
+type ClusterState string
+
 // MemberClusterSpec defines the desired state of MemberCluster. This is updated through the hub fleet api.
 type MemberClusterSpec struct {
-	// State indicates the state of the member cluster
+	// State indicates the desired state of the member cluster.
+
 	// +kubebuilder:validation:Enum=Join;Leave;Upgrade
 	// +required
 	State ClusterState `json:"state"`
 
-	// Identity used by the member cluster to contact the hub cluster
-	// The hub cluster will create the minimal required permission for this identity
+	// Identity used by the member cluster to contact the hub cluster.
+	// The hub cluster will create the minimal required permission for this identity.
 	// +required
 	Identity rbacv1.Subject `json:"identity"`
 
-	// HeartbeatPeriodSeconds indicates the lease update time for the member cluster agent.
-	// This is also the base of the threshold used by the Hub agent to determine if a member cluster is out of sync.
-	// If its value is zero, the member cluster agent will update its lease every 60 seconds by default
+	// HeartbeatPeriodSeconds indicates how often (in seconds) for the member cluster to send a heartbeat. Default to 60 seconds. Minimum value is 1.
+
 	// +optional
 	// +kubebuilder:default=60
 	HeartbeatPeriodSeconds int32 `json:"leaseDurationSeconds,omitempty"`
 }
 
-// MemberClusterStatus defines the observed state of MemberCluster. This is selectively copied from the
-// InternalMemberClusterStatus by the hub agent`1
+// MemberClusterStatus defines the observed state of MemberCluster.
 type MemberClusterStatus struct {
-	// Conditions field contains the different condition statuses for this member cluster.
+	// Conditions is an array of current observed conditions for this member cluster.
+
 	// +required
 	Conditions []metav1.Condition `json:"conditions"`
 
-	// Capacity represents the total resource capacity from all nodeStatuses
-	// on the member cluster.
+	// Capacity represents the total resources of all the nodes within the member cluster.
+
 	// +required
 	Capacity v1.ResourceList `json:"capacity"`
 
-	// Allocatable represents the total allocatable resources on the member cluster.
+	// Allocatable represents the total resources of all the nodes within the member cluster that are available for scheduling.
+
 	// +required
 	Allocatable v1.ResourceList `json:"allocatable"`
 }
 
 //+kubebuilder:object:root=true
 
-// MemberClusterList contains a list of MemberCluster
+// MemberClusterList contains a list of MemberCluster.
 type MemberClusterList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
