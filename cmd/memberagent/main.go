e803742040364802995880747e77ca56254e628f
Minh Nguyen <mnguyenn@protonmail.com>
2022-05-16 15:05:21 -0700

    Helm install doesn't work on custom namespace when not using clusterRole - attempt 1

--- cmd/memberagent/main.go
+++ cmd/memberagent/main.go
@@ -78,7 +78,7 @@ func main() {
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "InternalMemberCluster")
+		setupLog.Error(err, "unable to create controller", "controller", "InternalMemberClusterOnMember")
 		os.Exit(1)
 	}
 	//+kubebuilder:scaffold:builder
