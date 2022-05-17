6ec34207f66f514f3c03d4631a22a0e77ea31ce6
Minh Nguyen <mnguyenn@protonmail.com>
2022-05-17 10:12:13 -0700

    Setting up membership controller.

--- pkg/controllers/memberinternalmembercluster/member_controller.go
+++ pkg/controllers/memberinternalmembercluster/member_controller.go
@@ -7,10 +7,12 @@ package memberinternalmembercluster
 
 import (
 	"context"
+	"sync"
 
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/client-go/tools/record"
+	"k8s.io/klog/v2"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -19,20 +21,47 @@ import (
 	"go.goms.io/fleet/pkg/utils"
 )
 
+// TODO (mng): move `util` pkg to `controller/util`
+
 // MemberReconciler reconciles a InternalMemberCluster object in the member cluster.
 type MemberReconciler struct {
-	hubClient    client.Client
-	memberClient client.Client
-	restMapper   meta.RESTMapper
-	recorder     record.EventRecorder
+	hubClient                 client.Client
+	memberClient              client.Client
+	restMapper                meta.RESTMapper
+	recorder                  record.EventRecorder
+	internalMemberClusterChan chan<- fleetv1alpha1.ClusterState
+	membershipChan            <-chan fleetv1alpha1.ClusterState
 }
 
-func NewMemberReconciler(hubClient client.Client, memberClient client.Client,
-	restMapper meta.RESTMapper) *MemberReconciler {
+var membershipStateThreadSafe struct {
+	mu    sync.Mutex
+	state fleetv1alpha1.ClusterState
+}
+
+func watchMembershipChan(membershipChan <-chan fleetv1alpha1.ClusterState) {
+	for range membershipChan {
+		membershipStateSignal, more := <-membershipChan
+		if !more {
+			return
+		}
+		klog.InfoS("membership state",
+			"membership", membershipStateSignal)
+
+		membershipStateThreadSafe.mu.Lock()
+		membershipStateThreadSafe.state = membershipStateSignal
+		membershipStateThreadSafe.mu.Unlock()
+	}
+}
+
+func NewMemberReconciler(hubClient client.Client, memberClient client.Client, restMapper meta.RESTMapper,
+	internalMemberClusterChan chan<- fleetv1alpha1.ClusterState,
+	membershipChan <-chan fleetv1alpha1.ClusterState) *MemberReconciler {
 	return &MemberReconciler{
-		hubClient:    hubClient,
-		memberClient: memberClient,
-		restMapper:   restMapper,
+		hubClient:                 hubClient,
+		memberClient:              memberClient,
+		restMapper:                restMapper,
+		internalMemberClusterChan: internalMemberClusterChan,
+		membershipChan:            membershipChan,
 	}
 }
 
@@ -53,6 +82,7 @@ func (r *MemberReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 // SetupWithManager sets up the controller with the Manager.
 func (r *MemberReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	r.recorder = mgr.GetEventRecorderFor("InternalMemberCluster_member")
+	go watchMembershipChan(r.membershipChan)
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&fleetv1alpha1.InternalMemberCluster{}).
 		Complete(r)
