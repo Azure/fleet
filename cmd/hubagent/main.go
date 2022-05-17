f264a88c620301a19358b2d8363508c635acaa8d
Heba Elayoty <hebaelayoty@gmail.com>
2022-05-17 12:42:45 -0700

    Update main.go for both agents and add logs

--- cmd/hubagent/main.go
+++ cmd/hubagent/main.go
@@ -5,26 +5,30 @@ Licensed under the MIT license.
 package main
 
 import (
+	fleetv1alpha1 "github.com/Azure/fleet/apis/v1alpha1"
+	"github.com/Azure/fleet/pkg/controllers/internalmembercluster"
+	"github.com/Azure/fleet/pkg/controllers/membercluster"
+	"github.com/Azure/fleet/pkg/logger"
+
 	"flag"
 	"os"
-
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	fleetv1alpha1 "github.com/Azure/fleet/apis/v1alpha1"
-	"github.com/Azure/fleet/pkg/controllers/membercluster"
 	//+kubebuilder:scaffold:imports
 )
 
 var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
+	scheme               = runtime.NewScheme()
+	setupLog             = ctrl.Log.WithName("setup")
+	metricsAddr          = flag.String("metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	probeAddr            = flag.String("health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	enableLeaderElection = flag.Bool("leader-elect", false,
+		"Enable leader election for controllers membership and internalmembercluster. "+
+			"Enabling this will ensure there is only one active controllers membership and internalmembercluster.")
 )
 
 func init() {
@@ -35,40 +39,39 @@ func init() {
 }
 
 func main() {
-	var metricsAddr string
-	var enableLeaderElection bool
-	var probeAddr string
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
-		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
-	opts := zap.Options{
-		Development: true,
-	}
-	opts.BindFlags(flag.CommandLine)
 	flag.Parse()
 
-	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
-
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		Port:                   9443,
-		HealthProbeBindAddress: probeAddr,
-		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "984738fa.fleet.azure.com",
-	})
-	if err != nil {
-		setupLog.Error(err, "unable to start manager")
+	if err := logger.Configure(); err != nil {
+		setupLog.Error(err, "unable to configure logger")
 		os.Exit(1)
 	}
 
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme:                 scheme,
+		MetricsBindAddress:     *metricsAddr,
+		Port:                   9446,
+		HealthProbeBindAddress: *probeAddr,
+		LeaderElection:         *enableLeaderElection,
+		LeaderElectionID:       "994838fa.fleet.azure.com",
+	})
+	if err != nil {
+		setupLog.Error(err, "unable to start controllers membership and internalmembercluster.")
+		os.Exit(1)
+	}
+
+	setupLog.Info("setup membership and internalmembercluster controllers")
 	if err = (&membercluster.Reconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "MemberCluster")
+		os.Exit(1)
+	}
+	if err = (&internalmembercluster.HubReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "InternalMemberCluster for Hub")
 		os.Exit(1)
 	}
 	//+kubebuilder:scaffold:builder
@@ -82,7 +85,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	setupLog.Info("starting manager")
+	setupLog.Info("starting hubagent")
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
--- cmd/memberagent/main.go
+++ cmd/memberagent/main.go
@@ -6,6 +6,12 @@ Licensed under the MIT license.
 package main
 
 import (
+	fleetv1alpha1 "github.com/Azure/fleet/apis/v1alpha1"
+	"github.com/Azure/fleet/pkg/controllers/internalmembercluster"
+	"github.com/Azure/fleet/pkg/controllers/membership"
+
+	"github.com/Azure/fleet/pkg/logger"
+
 	"flag"
 	"os"
 
@@ -16,16 +22,17 @@ import (
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	fleetv1alpha1 "github.com/Azure/fleet/apis/v1alpha1"
-	"github.com/Azure/fleet/pkg/controllers/membercluster"
 	//+kubebuilder:scaffold:imports
 )
 
 var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
+	scheme               = runtime.NewScheme()
+	setupLog             = ctrl.Log.WithName("setup")
+	metricsAddr          = flag.String("metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	probeAddr            = flag.String("health-probe-bind-address", ":8082", "The address the probe endpoint binds to.")
+	enableLeaderElection = flag.Bool("leader-elect", false,
+		"Enable leader election for controllers membercluster and internalmembercluster. "+
+			"Enabling this will ensure there is only one active controllers membercluster and internalmembercluster.")
 )
 
 func init() {
@@ -36,40 +43,39 @@ func init() {
 }
 
 func main() {
-	var metricsAddr string
-	var enableLeaderElection bool
-	var probeAddr string
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
-		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
-	opts := zap.Options{
-		Development: true,
-	}
-	opts.BindFlags(flag.CommandLine)
 	flag.Parse()
 
-	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
-
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		Port:                   9443,
-		HealthProbeBindAddress: probeAddr,
-		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "984738fa.fleet.azure.com",
-	})
-	if err != nil {
-		setupLog.Error(err, "unable to start manager")
+	if err := logger.Configure(); err != nil {
+		setupLog.Error(err, "unable to configure logger")
 		os.Exit(1)
 	}
 
-	if err = (&membercluster.Reconciler{
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme:                 scheme,
+		MetricsBindAddress:     *metricsAddr,
+		Port:                   9443,
+		HealthProbeBindAddress: *probeAddr,
+		LeaderElection:         *enableLeaderElection,
+		LeaderElectionID:       "984738fa.fleet.azure.com",
+	})
+	if err != nil {
+		setupLog.Error(err, "unable to start membercluster and internalmembercluster.")
+		os.Exit(1)
+	}
+
+	setupLog.Info("setup membership and internalmembercluster controllers")
+	if err = (&membership.Reconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "MemberCluster")
+		os.Exit(1)
+	}
+	if err = (&internalmembercluster.MemberReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "InternalMemberCluster for Hub")
 		os.Exit(1)
 	}
 	//+kubebuilder:scaffold:builder
@@ -83,9 +89,9 @@ func main() {
 		os.Exit(1)
 	}
 
-	setupLog.Info("starting manager")
+	setupLog.Info("starting memberagent")
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-		setupLog.Error(err, "problem running manager")
+		setupLog.Error(err, "problem running membercluster and internalmembercluster")
 		os.Exit(1)
 	}
 }
