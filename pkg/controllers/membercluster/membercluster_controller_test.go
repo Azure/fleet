1a0ac5c6a8a9a02b00b7f67c56e286e301bc97a5
Arvind Thirumurugan <arvindthirumurugan@gmail.com>
2022-05-20 10:08:54 -0700

    Init Watch InternalMemberCluster

--- pkg/controllers/membercluster/membercluster_controller_test.go
+++ pkg/controllers/membercluster/membercluster_controller_test.go
@@ -18,6 +18,7 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/tools/record"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	fleetv1alpha1 "go.goms.io/fleet/apis/v1alpha1"
@@ -61,10 +62,13 @@ func TestReconcilerCheckAndCreateNamespace(t *testing.T) {
 	memberCluster3 := fleetv1alpha1.MemberCluster{ObjectMeta: metav1.ObjectMeta{Name: "mc3"}}
 	memberCluster4 := fleetv1alpha1.MemberCluster{ObjectMeta: metav1.ObjectMeta{Name: "mc4"}}
 
+	expectedEvent := utils.GetEventString(&memberCluster2, corev1.EventTypeNormal, namespaceCreated, "Namespace was created")
+
 	tests := map[string]struct {
 		r                   *Reconciler
 		memberCluster       *fleetv1alpha1.MemberCluster
 		wantedNamespaceName string
+		wantedEvent         string
 		wantedError         error
 	}{
 		"namespace exists": {
@@ -82,6 +86,7 @@ func TestReconcilerCheckAndCreateNamespace(t *testing.T) {
 			},
 			memberCluster:       &memberCluster2,
 			wantedNamespaceName: namespace2,
+			wantedEvent:         expectedEvent,
 			wantedError:         nil,
 		},
 		"namespace create error": {
@@ -105,6 +110,11 @@ func TestReconcilerCheckAndCreateNamespace(t *testing.T) {
 	for testName, tt := range tests {
 		t.Run(testName, func(t *testing.T) {
 			got, err := tt.r.checkAndCreateNamespace(context.Background(), tt.memberCluster)
+			if tt.r.recorder != nil {
+				fakeRecorder := tt.r.recorder.(*record.FakeRecorder)
+				event := <-fakeRecorder.Events
+				assert.Equal(t, tt.wantedEvent, event)
+			}
 			assert.Equal(t, tt.wantedError, err, utils.TestCaseMsg, testName)
 			assert.Equalf(t, tt.wantedNamespaceName, got, utils.TestCaseMsg, testName)
 		})
@@ -128,7 +138,7 @@ func TestReconcilerCheckAndCreateRole(t *testing.T) {
 			}
 			*o = rbacv1.Role{
 				TypeMeta: metav1.TypeMeta{
-					Kind:       RoleKind,
+					Kind:       "Role",
 					APIVersion: rbacv1.SchemeGroupVersion.String(),
 				},
 				ObjectMeta: metav1.ObjectMeta{
@@ -174,11 +184,15 @@ func TestReconcilerCheckAndCreateRole(t *testing.T) {
 	memberCluster5 := fleetv1alpha1.MemberCluster{ObjectMeta: metav1.ObjectMeta{Name: "mc5"}}
 	memberCluster6 := fleetv1alpha1.MemberCluster{ObjectMeta: metav1.ObjectMeta{Name: "mc6"}}
 
+	expectedEvent1 := utils.GetEventString(&memberCluster2, corev1.EventTypeNormal, roleUpdated, "role was updated")
+	expectedEvent2 := utils.GetEventString(&memberCluster3, corev1.EventTypeNormal, roleCreated, "role was created")
+
 	tests := map[string]struct {
 		r              *Reconciler
 		memberCluster  *fleetv1alpha1.MemberCluster
 		namespaceName  string
 		wantedRoleName string
+		wantedEvent    string
 		wantedError    error
 	}{
 		"role exists but no diff": {
@@ -198,6 +212,7 @@ func TestReconcilerCheckAndCreateRole(t *testing.T) {
 			memberCluster:  &memberCluster2,
 			namespaceName:  namespace2,
 			wantedRoleName: "fleet-role-mc2",
+			wantedEvent:    expectedEvent1,
 			wantedError:    nil,
 		},
 		"role doesn't exist": {
@@ -208,6 +223,7 @@ func TestReconcilerCheckAndCreateRole(t *testing.T) {
 			memberCluster:  &memberCluster3,
 			namespaceName:  namespace3,
 			wantedRoleName: "fleet-role-mc3",
+			wantedEvent:    expectedEvent2,
 			wantedError:    nil,
 		},
 		"role create error": {
@@ -242,6 +258,11 @@ func TestReconcilerCheckAndCreateRole(t *testing.T) {
 	for testName, tt := range tests {
 		t.Run(testName, func(t *testing.T) {
 			got, err := tt.r.checkAndCreateRole(context.Background(), tt.memberCluster, tt.namespaceName)
+			if tt.r.recorder != nil {
+				fakeRecorder := tt.r.recorder.(*record.FakeRecorder)
+				event := <-fakeRecorder.Events
+				assert.Equal(t, tt.wantedEvent, event)
+			}
 			assert.Equal(t, tt.wantedError, err, utils.TestCaseMsg, testName)
 			assert.Equalf(t, tt.wantedRoleName, got, utils.TestCaseMsg, testName)
 		})
@@ -259,13 +280,13 @@ func TestReconcilerCheckAndCreateRolebinding(t *testing.T) {
 		} else if key.Name == "fleet-rolebinding-mc1" && key.Namespace == namespace1 {
 			roleRef := rbacv1.RoleRef{
 				APIGroup: rbacv1.GroupName,
-				Kind:     RoleKind,
+				Kind:     "Role",
 				Name:     "fleet-role-mc1",
 			}
 			o := obj.(*rbacv1.RoleBinding)
 			*o = rbacv1.RoleBinding{
 				TypeMeta: metav1.TypeMeta{
-					Kind:       RoleBindingKind,
+					Kind:       "RoleBinding",
 					APIVersion: rbacv1.SchemeGroupVersion.String(),
 				},
 				ObjectMeta: metav1.ObjectMeta{
@@ -312,12 +333,16 @@ func TestReconcilerCheckAndCreateRolebinding(t *testing.T) {
 	memberCluster5 := fleetv1alpha1.MemberCluster{ObjectMeta: metav1.ObjectMeta{Name: "mc5"}}
 	memberCluster6 := fleetv1alpha1.MemberCluster{ObjectMeta: metav1.ObjectMeta{Name: "mc6"}}
 
+	expectedEvent1 := utils.GetEventString(&memberCluster2, corev1.EventTypeNormal, roleBindingUpdated, "role binding was updated")
+	expectedEvent2 := utils.GetEventString(&memberCluster3, corev1.EventTypeNormal, roleBindingCreated, "role binding was created")
+
 	tests := map[string]struct {
 		r             *Reconciler
 		memberCluster *fleetv1alpha1.MemberCluster
 		namespaceName string
 		roleName      string
 		identity      rbacv1.Subject
+		wantedEvent   string
 		wantedError   error
 	}{
 		"role binding but no diff": {
@@ -339,6 +364,7 @@ func TestReconcilerCheckAndCreateRolebinding(t *testing.T) {
 			namespaceName: namespace2,
 			roleName:      "fleet-role-mc2",
 			identity:      identity,
+			wantedEvent:   expectedEvent1,
 			wantedError:   nil,
 		},
 		"role binding doesn't exist": {
@@ -350,6 +376,7 @@ func TestReconcilerCheckAndCreateRolebinding(t *testing.T) {
 			namespaceName: namespace3,
 			roleName:      "fleet-role-mc3",
 			identity:      identity,
+			wantedEvent:   expectedEvent2,
 			wantedError:   nil,
 		},
 		"role binding create error": {
@@ -387,6 +414,94 @@ func TestReconcilerCheckAndCreateRolebinding(t *testing.T) {
 	for testName, tt := range tests {
 		t.Run(testName, func(t *testing.T) {
 			err := tt.r.checkAndCreateRoleBinding(context.Background(), tt.memberCluster, tt.namespaceName, tt.roleName, identity)
+			if tt.r.recorder != nil {
+				fakeRecorder := tt.r.recorder.(*record.FakeRecorder)
+				event := <-fakeRecorder.Events
+				assert.Equal(t, tt.wantedEvent, event)
+			}
+			assert.Equal(t, tt.wantedError, err, utils.TestCaseMsg, testName)
+		})
+	}
+}
+
+func TestCheckAndCreateInternalMemberCluster(t *testing.T) {
+	getMock := func(ctx context.Context, key client.ObjectKey, obj client.Object) error {
+		if key.Name == "mc2" && key.Namespace == namespace2 || key.Name == "mc3" && key.Namespace == namespace3 {
+			return apierrors.NewNotFound(schema.GroupResource{Group: "", Resource: "Namespace"}, "namespace")
+		} else if key.Name == "mc1" && key.Namespace == namespace1 {
+			o := obj.(*fleetv1alpha1.InternalMemberCluster)
+			*o = fleetv1alpha1.InternalMemberCluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "mc1",
+					Namespace: namespace1,
+				},
+			}
+		} else if key.Name == "mc4" && key.Namespace == namespace4 {
+			return fmt.Errorf("internal member cluster cannot be retrieved")
+		}
+		return nil
+	}
+
+	createMock := func(ctx context.Context, obj client.Object, opts ...client.CreateOption) error {
+		o := obj.(*fleetv1alpha1.InternalMemberCluster)
+		if o.Name == "mc2" && o.Namespace == namespace2 {
+			return nil
+		}
+		return fmt.Errorf("internal member cluster cannot be updated")
+	}
+
+	memberCluster1 := fleetv1alpha1.MemberCluster{ObjectMeta: metav1.ObjectMeta{Name: "mc1"}}
+	memberCluster2 := fleetv1alpha1.MemberCluster{ObjectMeta: metav1.ObjectMeta{Name: "mc2"}}
+	memberCluster3 := fleetv1alpha1.MemberCluster{ObjectMeta: metav1.ObjectMeta{Name: "mc3"}}
+	memberCluster4 := fleetv1alpha1.MemberCluster{ObjectMeta: metav1.ObjectMeta{Name: "mc4"}}
+
+	expectedEvent := utils.GetEventString(&memberCluster2, corev1.EventTypeNormal, internalMemberClusterCreated, "Internal member cluster was created")
+
+	tests := map[string]struct {
+		r             *Reconciler
+		memberCluster *fleetv1alpha1.MemberCluster
+		namespaceName string
+		wantedEvent   string
+		wantedError   error
+	}{
+		"internal member cluster exists": {
+			r:             &Reconciler{Client: &test.MockClient{MockGet: getMock}},
+			memberCluster: &memberCluster1,
+			namespaceName: namespace1,
+			wantedError:   nil,
+		},
+		"internal member cluster doesn't exist": {
+			r: &Reconciler{
+				Client:   &test.MockClient{MockGet: getMock, MockCreate: createMock},
+				recorder: utils.NewFakeRecorder(1),
+			},
+			memberCluster: &memberCluster2,
+			namespaceName: namespace2,
+			wantedEvent:   expectedEvent,
+			wantedError:   nil,
+		},
+		"internal member create error": {
+			r:             &Reconciler{Client: &test.MockClient{MockGet: getMock, MockCreate: createMock}},
+			memberCluster: &memberCluster3,
+			namespaceName: namespace3,
+			wantedError:   errors.New("internal member cluster cannot be updated"),
+		},
+		"internal member get error": {
+			r:             &Reconciler{Client: &test.MockClient{MockGet: getMock}},
+			memberCluster: &memberCluster4,
+			namespaceName: namespace4,
+			wantedError:   errors.New("internal member cluster cannot be retrieved"),
+		},
+	}
+
+	for testName, tt := range tests {
+		t.Run(testName, func(t *testing.T) {
+			err := tt.r.checkAndCreateInternalMemberCluster(context.Background(), tt.memberCluster, tt.namespaceName)
+			if tt.r.recorder != nil {
+				fakeRecorder := tt.r.recorder.(*record.FakeRecorder)
+				event := <-fakeRecorder.Events
+				assert.Equal(t, tt.wantedEvent, event)
+			}
 			assert.Equal(t, tt.wantedError, err, utils.TestCaseMsg, testName)
 		})
 	}
--- pkg/utils/test_util.go
+++ pkg/utils/test_util.go
@@ -1,14 +1,32 @@
+/*
+Copyright (c) Microsoft Corporation.
+Licensed under the MIT license.
+*/
+
 package utils
 
-import "k8s.io/client-go/tools/record"
+import (
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/record"
+)
 
 const (
 	// TestCaseMsg is used in the table driven test
 	TestCaseMsg string = "\nTest case:  %s"
 )
 
+// NewFakeRecorder makes a new fake event recorder that prints the object.
 func NewFakeRecorder(bufferSize int) *record.FakeRecorder {
 	recorder := record.NewFakeRecorder(bufferSize)
 	recorder.IncludeObject = true
 	return recorder
+}
+
+// GetEventString get the exact string literal of the event created by the fake event library.
+func GetEventString(object runtime.Object, eventtype, reason, messageFmt string, args ...interface{}) string {
+	return fmt.Sprintf(eventtype+" "+reason+" "+messageFmt, args...) +
+		fmt.Sprintf(" involvedObject{kind=%s,apiVersion=%s}",
+			object.GetObjectKind().GroupVersionKind().Kind, object.GetObjectKind().GroupVersionKind().GroupVersion())
 }
