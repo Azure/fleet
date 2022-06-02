5a2664cddc62addb877bd5e3d881a7eacb8db570
Minh Nguyen <mnguyenn@protonmail.com>
2022-06-02 14:12:51 -0700

    feat: Add logic to mark internal member cluster heartbeat condition as received/ unknown. (#44)

--- pkg/controllers/memberinternalmembercluster/member_controller_test.go
+++ pkg/controllers/memberinternalmembercluster/member_controller_test.go
@@ -87,3 +87,50 @@ func TestMarkInternalMemberClusterUnknown(t *testing.T) {
 		assert.Equal(t, "", cmp.Diff(expectedCondition, *(actualCondition), cmpopts.IgnoreTypes(time.Time{})), utils.TestCaseMsg, "TestMarkInternalMemberClusterUnknown")
 	}
 }
+
+func TestMarkInternalMemberClusterHeartbeatReceived(t *testing.T) {
+	r := Reconciler{recorder: utils.NewFakeRecorder(1)}
+	internalMemberCluster := &v1alpha1.InternalMemberCluster{}
+
+	r.markInternalMemberClusterHeartbeatReceived(internalMemberCluster)
+
+	// check that the correct event is emitted
+	event := <-r.recorder.(*record.FakeRecorder).Events
+	expected := utils.GetEventString(internalMemberCluster, v12.EventTypeNormal, eventReasonInternalMemberClusterHBReceived, "internal member cluster heartbeat received")
+
+	assert.Equal(t, expected, event, utils.TestCaseMsg, "TestMarkInternalMemberClusterHeartbeatReceived")
+
+	// Check expected conditions.
+	expectedConditions := []metav1.Condition{
+		{Type: v1alpha1.ConditionTypeInternalMemberClusterHeartbeat, Status: metav1.ConditionTrue, Reason: eventReasonInternalMemberClusterHBReceived},
+		{Type: common.ConditionTypeSynced, Status: metav1.ConditionTrue, Reason: common.ReasonReconcileSuccess},
+	}
+
+	for _, expectedCondition := range expectedConditions {
+		actualCondition := internalMemberCluster.GetCondition(expectedCondition.Type)
+		assert.Equal(t, "", cmp.Diff(expectedCondition, *(actualCondition), cmpopts.IgnoreTypes(time.Time{})), utils.TestCaseMsg, "TestMarkInternalMemberClusterHeartbeatReceived")
+	}
+}
+
+func TestMarkInternalMemberClusterHeartbeatUnknown(t *testing.T) {
+	internalMemberCluster := &v1alpha1.InternalMemberCluster{}
+	r := Reconciler{recorder: utils.NewFakeRecorder(1)}
+
+	r.markInternalMemberClusterHeartbeatUnknown(internalMemberCluster)
+
+	// check that the correct event is emitted
+	event := <-r.recorder.(*record.FakeRecorder).Events
+	expected := utils.GetEventString(internalMemberCluster, core.EventTypeNormal, eventReasonInternalMemberClusterHBUnknown, "internal member cluster heartbeat unknown")
+
+	assert.Equal(t, expected, event, utils.TestCaseMsg, "TestMarkInternalMemberClusterHeartbeatUnknown")
+
+	// Check expected conditions.
+	expectedConditions := []metav1.Condition{
+		{Type: v1alpha1.ConditionTypeInternalMemberClusterHeartbeat, Status: metav1.ConditionUnknown, Reason: eventReasonInternalMemberClusterHBUnknown},
+		{Type: common.ConditionTypeSynced, Status: metav1.ConditionTrue, Reason: common.ReasonReconcileSuccess},
+	}
+	for _, expectedCondition := range expectedConditions {
+		actualCondition := internalMemberCluster.GetCondition(expectedCondition.Type)
+		assert.Equal(t, "", cmp.Diff(expectedCondition, *(actualCondition), cmpopts.IgnoreTypes(time.Time{})), utils.TestCaseMsg, "TestMarkInternalMemberClusterHeartbeatUnknown")
+	}
+}
