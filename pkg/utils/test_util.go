2f64e62774751b2ff11fe6479579d7a069d566b2
Minh Nguyen <mnguyenn@protonmail.com>
2022-06-14 14:16:36 -0700

    feat: Init test for Internal Member Cluster controller. (#94)

--- pkg/utils/test_util.go
+++ pkg/utils/test_util.go
@@ -7,7 +7,12 @@ package utils
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/tools/record"
 )
@@ -29,4 +34,31 @@ func GetEventString(object runtime.Object, eventtype, reason, messageFmt string,
 	return fmt.Sprintf(eventtype+" "+reason+" "+messageFmt, args...) +
 		fmt.Sprintf(" involvedObject{kind=%s,apiVersion=%s}",
 			object.GetObjectKind().GroupVersionKind().Kind, object.GetObjectKind().GroupVersionKind().GroupVersion())
+}
+
+// NewTestNodes return a set of nodes for test purpose. Those nodes have random names and capacities/ allocatable.
+func NewTestNodes(ns string) []v1.Node {
+	numOfNodes := RandSecureInt(10)
+	var nodes []v1.Node
+	for i := int64(0); i < numOfNodes; i++ {
+		allocatableCPU := RandSecureInt(100)
+		allocatableMemory := RandSecureInt(5)
+		capacityCPU := RandSecureInt(100)
+		capacityMemory := RandSecureInt(5)
+		node := v1.Node{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "rand-" + strings.ToLower(RandStr()) + "-node",
+				Namespace: ns,
+			},
+			Status: v1.NodeStatus{Allocatable: v1.ResourceList{
+				v1.ResourceCPU:    resource.MustParse(strconv.FormatInt(allocatableCPU, 10) + "m"),
+				v1.ResourceMemory: resource.MustParse(strconv.FormatInt(allocatableMemory, 10) + "G"),
+			}, Capacity: v1.ResourceList{
+				v1.ResourceCPU:    resource.MustParse(strconv.FormatInt(capacityCPU, 10) + "m"),
+				v1.ResourceMemory: resource.MustParse(strconv.FormatInt(capacityMemory, 10) + "G"),
+			}},
+		}
+		nodes = append(nodes, node)
+	}
+	return nodes
 }
