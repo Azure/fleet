f03a97252c95b98a715a6c215469fe11b1168e8b
Minh Nguyen <mnguyenn@protonmail.com>
2022-06-18 23:59:47 -0700

    feat: Test metrics exposure. (#107)

---
+++ test/e2e/metrics_exposure_test.go
@@ -0,0 +1,45 @@
+package e2e
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"go.goms.io/fleet/test/e2e/framework"
+	"k8s.io/client-go/kubernetes"
+)
+
+// TODO (mng): move this test to join/leave tests after those tests finished.
+var _ = Describe("Test metrics exposure", func() {
+	It("check exposed metrics on hub cluster", func() {
+		By("creating cluster REST config")
+		clusterConfig := framework.GetClientConfig(HubCluster)
+		restConfig, err := clusterConfig.ClientConfig()
+		Expect(err).ToNot(HaveOccurred())
+
+		By("creating cluster clientSet")
+		clientSet, err := kubernetes.NewForConfig(restConfig)
+		Expect(err).ToNot(HaveOccurred())
+
+		By("getting metrics exposed at /metrics endpoint")
+		metrics, err := clientSet.RESTClient().Get().AbsPath("/metrics").DoRaw(context.Background())
+		Expect(err).ToNot(HaveOccurred())
+		Expect(metrics).ToNot(BeEmpty())
+	})
+
+	It("check exposed metrics on member cluster", func() {
+		By("creating cluster REST config")
+		clusterConfig := framework.GetClientConfig(MemberCluster)
+		restConfig, err := clusterConfig.ClientConfig()
+		Expect(err).ToNot(HaveOccurred())
+
+		By("creating cluster clientSet")
+		clientSet, err := kubernetes.NewForConfig(restConfig)
+		Expect(err).ToNot(HaveOccurred())
+
+		By("getting metrics exposed at /metrics endpoint")
+		data, err := clientSet.RESTClient().Get().AbsPath("/metrics").DoRaw(context.Background())
+		Expect(err).ToNot(HaveOccurred())
+		Expect(data).ToNot(BeEmpty())
+	})
+})
