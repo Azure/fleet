1cdd5362e5c9589d29285cacb62f0ef13d4ba159
Minh Nguyen <mnguyenn@protonmail.com>
2022-06-30 13:30:52 -0700

    Update typo

--- test/e2e/join_leave_member_test.go
+++ test/e2e/join_leave_member_test.go
@@ -97,7 +97,7 @@ var _ = Describe("Join/leave member cluster testing", func() {
 				framework.WaitConditionMemberCluster(*HubCluster, mc, v1alpha1.ConditionTypeMemberClusterJoin, v1.ConditionFalse, 3*framework.PollTimeout)
 			})
 
-			By("check if internalMemberCluster condition is updated to Joined", func() {
+			By("check if internalMemberCluster condition is updated to Left", func() {
 				framework.WaitConditionInternalMemberCluster(*HubCluster, imc, v1alpha1.ConditionTypeInternalMemberClusterJoin, v1.ConditionFalse, 3*framework.PollTimeout)
 			})
 
