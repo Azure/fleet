8afbfcbe7eb9877675b0ffab519998aef5ba8dec
Ryan Zhang <yangzhangrice@hotmail.com>
2022-07-07 13:41:51 -0700

    feat: add disabled api resource list util (#160)
    * feat: add disabled api resource lib
    
    * change the interface to work wtih GVK only
    
    Co-authored-by: Ryan Zhang <zhangryan@microsoft.com>

---
+++ pkg/utils/apiresources.go
@@ -0,0 +1,178 @@
+/*
+Copyright (c) Microsoft Corporation.
+Licensed under the MIT license.
+*/
+
+package utils
+
+import (
+	"fmt"
+	"strings"
+
+	corev1 "k8s.io/api/core/v1"
+	eventsv1 "k8s.io/api/events/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	fleetv1alpha1 "go.goms.io/fleet/apis/v1alpha1"
+)
+
+var (
+	// TODO: add more default resource configs to skip
+	corev1PodGVK = schema.GroupVersionKind{
+		Group:   corev1.GroupName,
+		Version: corev1.SchemeGroupVersion.Version,
+		Kind:    "Pod",
+	}
+
+	corev1NodeGVK = schema.GroupVersionKind{
+		Group:   corev1.GroupName,
+		Version: corev1.SchemeGroupVersion.Version,
+		Kind:    "Node",
+	}
+
+	// we use `;` to separate the different api groups
+	apiGroupSepToken = ";"
+)
+
+// DisabledResourceConfig represents the configuration that identifies the API resources should not be selected.
+type DisabledResourceConfig struct {
+	// groups holds a collection of API group, all resources under this group will be avoided.
+	groups map[string]struct{}
+	// groupVersions holds a collection of API GroupVersion, all resource under this GroupVersion will be avoided.
+	groupVersions map[schema.GroupVersion]struct{}
+	// groupVersionKinds holds a collection of resource that should be avoided.
+	groupVersionKinds map[schema.GroupVersionKind]struct{}
+}
+
+// NewDisabledResourceConfig to create DisabledResourceConfig
+func NewDisabledResourceConfig() *DisabledResourceConfig {
+	r := &DisabledResourceConfig{
+		groups:            map[string]struct{}{},
+		groupVersions:     map[schema.GroupVersion]struct{}{},
+		groupVersionKinds: map[schema.GroupVersionKind]struct{}{},
+	}
+	// disable fleet group by default
+	r.DisableGroup(fleetv1alpha1.GroupVersion.Group)
+	// disable event by default
+	r.DisableGroup(eventsv1.GroupName)
+	r.DisableGroupVersionKind(corev1PodGVK)
+	r.DisableGroupVersionKind(corev1NodeGVK)
+	return r
+}
+
+// Parse parses the --avoid-selecting-apis input.
+func (r *DisabledResourceConfig) Parse(c string) error {
+	// default(empty) input
+	if c == "" {
+		return nil
+	}
+
+	tokens := strings.Split(c, apiGroupSepToken)
+	for _, token := range tokens {
+		if err := r.parseSingle(token); err != nil {
+			return fmt.Errorf("parse --avoid-selecting-apis %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (r *DisabledResourceConfig) parseSingle(token string) error {
+	switch strings.Count(token, "/") {
+	// Assume user don't want to skip the 'core'(no group name) group.
+	// So, it should be the case "<group>".
+	case 0:
+		r.groups[token] = struct{}{}
+	// it should be the case "<group>/<version>"
+	case 1:
+		// for core group which don't have the group name, the case should be "v1/<kind>" or "v1/<kind>,<kind>..."
+		if strings.HasPrefix(token, "v1") {
+			var kinds []string
+			for _, k := range strings.Split(token, ",") {
+				if strings.Contains(k, "/") { // "v1/<kind>"
+					s := strings.Split(k, "/")
+					kinds = append(kinds, s[1])
+				} else {
+					kinds = append(kinds, k)
+				}
+			}
+			for _, k := range kinds {
+				gvk := schema.GroupVersionKind{
+					Version: "v1",
+					Kind:    k,
+				}
+				r.groupVersionKinds[gvk] = struct{}{}
+			}
+		} else { // case "<group>/<version>"
+			parts := strings.Split(token, "/")
+			if len(parts) != 2 {
+				return fmt.Errorf("invalid token: %s", token)
+			}
+			gv := schema.GroupVersion{
+				Group:   parts[0],
+				Version: parts[1],
+			}
+			r.groupVersions[gv] = struct{}{}
+		}
+	// parameter format: "<group>/<version>/<kind>" or "<group>/<version>/<kind>,<kind>..."
+	case 2:
+		g := ""
+		v := ""
+		var kinds []string
+		for _, k := range strings.Split(token, ",") {
+			if strings.Contains(k, "/") {
+				s := strings.Split(k, "/")
+				g = s[0]
+				v = s[1]
+				kinds = append(kinds, s[2])
+			} else {
+				kinds = append(kinds, k)
+			}
+		}
+		for _, k := range kinds {
+			gvk := schema.GroupVersionKind{
+				Group:   g,
+				Version: v,
+				Kind:    k,
+			}
+			r.groupVersionKinds[gvk] = struct{}{}
+		}
+	default:
+		return fmt.Errorf("invalid parameter: %s", token)
+	}
+
+	return nil
+}
+
+// IsResourceDisabled returns whether a given GroupVersionKind is disabled.
+// a gkv is disabled if its group or group version is disabled
+func (r *DisabledResourceConfig) IsResourceDisabled(gvk schema.GroupVersionKind) bool {
+	if _, ok := r.groups[gvk.Group]; ok {
+		return true
+	}
+
+	if _, ok := r.groupVersions[gvk.GroupVersion()]; ok {
+		return true
+	}
+
+	if _, ok := r.groupVersionKinds[gvk]; ok {
+		return true
+	}
+
+	return false
+}
+
+// DisableGroup to disable group.
+func (r *DisabledResourceConfig) DisableGroup(g string) {
+	r.groups[g] = struct{}{}
+}
+
+// DisableGroupVersion to disable group version.
+func (r *DisabledResourceConfig) DisableGroupVersion(gv schema.GroupVersion) {
+	r.groupVersions[gv] = struct{}{}
+}
+
+// DisableGroupVersionKind to disable GroupVersionKind.
+func (r *DisabledResourceConfig) DisableGroupVersionKind(gvk schema.GroupVersionKind) {
+	r.groupVersionKinds[gvk] = struct{}{}
+}
---
+++ pkg/utils/apiresources_test.go
@@ -0,0 +1,371 @@
+/*
+Copyright (c) Microsoft Corporation.
+Licensed under the MIT license.
+*/
+
+package utils
+
+import (
+	"testing"
+
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+func TestResourceConfigGVKParse(t *testing.T) {
+	tests := []struct {
+		input    string
+		disabled []schema.GroupVersionKind
+		enabled  []schema.GroupVersionKind
+	}{
+		{
+			input: "v1/Node,Pod;networking.k8s.io/v1beta1/Ingress,IngressClass",
+			disabled: []schema.GroupVersionKind{
+				{
+					Group:   "",
+					Version: "v1",
+					Kind:    "Node",
+				},
+				{
+					Group:   "",
+					Version: "v1",
+					Kind:    "Pod",
+				},
+				{
+					Group:   "networking.k8s.io",
+					Version: "v1beta1",
+					Kind:    "Ingress",
+				},
+				{
+					Group:   "networking.k8s.io",
+					Version: "v1beta1",
+					Kind:    "IngressClass",
+				},
+			},
+			enabled: []schema.GroupVersionKind{
+				{
+					Group:   "",
+					Version: "v1",
+					Kind:    "ResourceQuota",
+				},
+				{
+					Group:   "apps",
+					Version: "v1",
+					Kind:    "ControllerRevision",
+				},
+				{
+					Group:   "networking.k8s.io",
+					Version: "v1",
+					Kind:    "Ingress",
+				},
+				{
+					Group:   "certificates.k8s.io",
+					Version: "v1beta1",
+					Kind:    "CertificateSigningRequest",
+				},
+				{
+					Group:   "networking.k8s.io",
+					Version: "v1beta1",
+					Kind:    "",
+				},
+			},
+		},
+	}
+	for _, test := range tests {
+		r := NewDisabledResourceConfig()
+		if err := r.Parse(test.input); err != nil {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+		for i, o := range test.disabled {
+			ok := r.IsResourceDisabled(o)
+			if !ok {
+				t.Errorf("%d: unexpected error: %v", i, o)
+			}
+		}
+		for i, o := range test.enabled {
+			ok := r.IsResourceDisabled(o)
+			if ok {
+				t.Errorf("%d: unexpected error: %v", i, o)
+			}
+		}
+	}
+}
+
+func TestResourceConfigGVParse(t *testing.T) {
+	tests := []struct {
+		input    string
+		disabled []schema.GroupVersionKind
+		enabled  []schema.GroupVersionKind
+	}{
+		{
+			input: "networking.k8s.io/v1;test/v1beta1",
+			disabled: []schema.GroupVersionKind{
+				{
+					Group:   "networking.k8s.io",
+					Version: "v1",
+					Kind:    "Ingress",
+				},
+				{
+					Group:   "networking.k8s.io",
+					Version: "v1",
+					Kind:    "EgressClass",
+				},
+				{
+					Group:   "test",
+					Version: "v1beta1",
+					Kind:    "Lease",
+				},
+				{
+					Group:   "test",
+					Version: "v1beta1",
+					Kind:    "HealthState",
+				},
+			},
+			enabled: []schema.GroupVersionKind{
+				{
+					Group:   "networking.k8s.io",
+					Version: "v1beta1",
+					Kind:    "Ingress",
+				},
+				{
+					Group:   "networking.k8s.io",
+					Version: "v1beta1",
+					Kind:    "IngressClass",
+				},
+				{
+					Group:   "test",
+					Version: "v1",
+					Kind:    "Service",
+				},
+			},
+		},
+	}
+	for _, test := range tests {
+		r := NewDisabledResourceConfig()
+		if err := r.Parse(test.input); err != nil {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+		for i, o := range test.disabled {
+			ok := r.IsResourceDisabled(o)
+			if !ok {
+				t.Errorf("%d: unexpected error: %v", i, o)
+			}
+		}
+		for i, o := range test.enabled {
+			ok := r.IsResourceDisabled(o)
+			if ok {
+				t.Errorf("%d: unexpected error: %v", i, o)
+			}
+		}
+	}
+}
+
+func TestResourceConfigGroupParse(t *testing.T) {
+	tests := []struct {
+		input    string
+		disabled []schema.GroupVersionKind
+		enabled  []schema.GroupVersionKind
+	}{
+		{
+			input: "networking.k8s.io;apps;secrets-store.csi.x-k8s.io",
+			disabled: []schema.GroupVersionKind{
+				{
+					Group:   "networking.k8s.io",
+					Version: "v1",
+					Kind:    "Ingress",
+				},
+				{
+					Group:   "networking.k8s.io",
+					Version: "v1beta1",
+					Kind:    "EgressClass",
+				},
+				{
+					Group:   "apps",
+					Version: "v1beta1",
+					Kind:    "Lease",
+				},
+				{
+					Group:   "secrets-store.csi.x-k8s.io",
+					Version: "v1beta1",
+					Kind:    "HealthState",
+				},
+			},
+			enabled: []schema.GroupVersionKind{
+				{
+					Group:   "",
+					Version: "v1beta1",
+					Kind:    "Ingress",
+				},
+				{
+					Group:   "apiregistration.k8s.io",
+					Version: "v1beta1",
+					Kind:    "IngressClass",
+				},
+				{
+					Group:   "authentication.k8s.i",
+					Version: "v1",
+					Kind:    "Service",
+				},
+			},
+		},
+	}
+	for _, test := range tests {
+		r := NewDisabledResourceConfig()
+		if err := r.Parse(test.input); err != nil {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+		for i, o := range test.disabled {
+			ok := r.IsResourceDisabled(o)
+			if !ok {
+				t.Errorf("%d: unexpected error: %v", i, o)
+			}
+		}
+		for i, o := range test.enabled {
+			ok := r.IsResourceDisabled(o)
+			if ok {
+				t.Errorf("%d: unexpected error: %v", i, o)
+			}
+		}
+	}
+}
+
+func TestDisabledResourceConfigMixedParse(t *testing.T) {
+	tests := []struct {
+		input    string
+		disabled []schema.GroupVersionKind
+		enabled  []schema.GroupVersionKind
+	}{
+		{
+			input: "v1/Node,Pod;networking.k8s.io;apps/v1;authorization.k8s.io/v1/SelfSubjectRulesReview",
+			disabled: []schema.GroupVersionKind{
+				{
+					Group:   "networking.k8s.io",
+					Version: "v1beta1",
+					Kind:    "Ingress",
+				},
+				{
+					Group:   "networking.k8s.io",
+					Version: "v1",
+					Kind:    "IngressClass",
+				},
+				{
+					Group:   "",
+					Version: "v1",
+					Kind:    "Node",
+				},
+				{
+					Group:   "",
+					Version: "v1",
+					Kind:    "Pod",
+				},
+				{
+					Group:   "authorization.k8s.io",
+					Version: "v1",
+					Kind:    "SelfSubjectRulesReview",
+				},
+				{
+					Group:   "apps",
+					Version: "v1",
+					Kind:    "HealthState",
+				},
+			},
+			enabled: []schema.GroupVersionKind{
+				{
+					Group:   "",
+					Version: "v1",
+					Kind:    "Ingress",
+				},
+				{
+					Group:   "apps",
+					Version: "v1beta1",
+					Kind:    "IngressClass",
+				},
+				{
+					Group:   "authorization.k8s.io",
+					Version: "v1",
+					Kind:    "LocalSubjectAccessReview",
+				},
+			},
+		},
+	}
+	for _, test := range tests {
+		r := NewDisabledResourceConfig()
+		if err := r.Parse(test.input); err != nil {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+		for i, o := range test.disabled {
+			ok := r.IsResourceDisabled(o)
+			if !ok {
+				t.Errorf("%d: unexpected error: %v", i, o)
+			}
+		}
+		for i, o := range test.enabled {
+			ok := r.IsResourceDisabled(o)
+			if ok {
+				t.Errorf("%d: unexpected error: %v", i, o)
+			}
+		}
+	}
+}
+
+func TestDefaultDisabledResourceConfigGroupVersionKindParse(t *testing.T) {
+	tests := []struct {
+		disabled []schema.GroupVersionKind
+		enabled  []schema.GroupVersionKind
+	}{
+		{
+			disabled: []schema.GroupVersionKind{
+				corev1PodGVK, corev1NodeGVK,
+				{
+					Group:   "fleet.azure.com",
+					Version: "v1beta1",
+					Kind:    "MemberCluster",
+				},
+				{
+					Group:   "fleet.azure.com",
+					Version: "v1alpha1",
+					Kind:    "MemberCluster",
+				},
+				{
+					Group:   "events.k8s.io",
+					Version: "v1beta1",
+					Kind:    "Event",
+				},
+			},
+			enabled: []schema.GroupVersionKind{
+				{
+					Group:   "",
+					Version: "v1",
+					Kind:    "Namespace",
+				},
+				{
+					Group:   "apps",
+					Version: "v1",
+					Kind:    "Deployment",
+				},
+				{
+					Group:   "",
+					Version: "v1",
+					Kind:    "Event",
+				},
+			},
+		},
+	}
+	for _, test := range tests {
+		r := NewDisabledResourceConfig()
+		if err := r.Parse(""); err != nil {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+		for i, o := range test.disabled {
+			ok := r.IsResourceDisabled(o)
+			if !ok {
+				t.Errorf("%d: unexpected error: %v", i, o)
+			}
+		}
+		for i, o := range test.enabled {
+			ok := r.IsResourceDisabled(o)
+			if ok {
+				t.Errorf("%d: unexpected error: %v", i, o)
+			}
+		}
+	}
+}
