51c8d083179de323306706371c5fb15bc76610f2
Ryan Zhang <zhangryan@microsoft.com>
2022-07-07 10:33:16 -0700

    add original license

--- pkg/utils/native.go
+++ pkg/utils/native.go
@@ -1,6 +1,9 @@
 /*
 Copyright (c) Microsoft Corporation.
 Licensed under the MIT license.
+
+Copyright 2016 The Kubernetes Authors.
+Licensed under the Apache License, Version 2.0.
 */
 
 package utils
