312d0381c0e86196c3b1da15fa1f71a5bb5534e2
Arvind Thirumurugan <arvindthirumurugan@gmail.com>
2022-08-16 00:11:36 -0700

    add afterEach

--- test/e2e/work_load_test.go
+++ test/e2e/work_load_test.go
@@ -1,15 +1,21 @@
 package e2e
 
 import (
+	"context"
 	"fmt"
 
 	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	workapi "sigs.k8s.io/work-api/pkg/apis/v1alpha1"
+
 	"go.goms.io/fleet/apis/v1alpha1"
 	"go.goms.io/fleet/pkg/utils"
 	"go.goms.io/fleet/test/e2e/framework"
-	corev1 "k8s.io/api/core/v1"
-	rbacv1 "k8s.io/api/rbac/v1"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = Describe("workload orchestration testing", func() {
@@ -20,8 +26,12 @@ var _ = Describe("workload orchestration testing", func() {
 	var imc *v1alpha1.InternalMemberCluster
 	var cr *rbacv1.ClusterRole
 	var crp *v1alpha1.ClusterResourcePlacement
+	var work *workapi.Work
 
 	memberNS = NewNamespace(fmt.Sprintf(utils.NamespaceNameFormat, MemberCluster.ClusterName))
+	clusterRoleName := "test-cluster-role"
+	placementName := "resource-label-selector"
+	workName := fmt.Sprintf(utils.WorkNameFormat, placementName)
 
 	BeforeEach(func() {
 		memberIdentity = rbacv1.Subject{
@@ -34,7 +44,6 @@ var _ = Describe("workload orchestration testing", func() {
 	It("Join flow is successful ", func() {
 		By("Prepare resources in member cluster", func() {
 			// create testing NS in member cluster
-			//framework.DeleteNamespace(*MemberCluster, memberNS)
 			framework.CreateNamespace(*MemberCluster, memberNS)
 			framework.WaitNamespace(*MemberCluster, memberNS)
 
@@ -63,36 +72,41 @@ var _ = Describe("workload orchestration testing", func() {
 		})
 
 		By("create the resources to be propagated", func() {
-			cr = NewClusterRole("test-cluster-role")
+			cr = NewClusterRole(clusterRoleName)
 			framework.CreateClusterRole(*HubCluster, cr)
 		})
 
 		By("create the cluster resource placement in the hub cluster", func() {
-			crp = NewClusterResourcePlacement("resource-label-selector")
+			crp = NewClusterResourcePlacement(placementName)
 			framework.CreateClusterResourcePlacement(*HubCluster, crp)
 			framework.WaitClusterResourcePlacement(*HubCluster, crp)
 		})
 
-		By("check if cluster resource placement is updated to Scheduled", func() {
-			framework.WaitConditionClusterResourcePlacement(*HubCluster, crp, string(v1alpha1.ResourcePlacementConditionTypeScheduled), v1.ConditionTrue, 3*framework.PollTimeout)
+		By("check if work get created for cluster resource placement", func() {
+			work = framework.GetWork(*HubCluster, workName, memberNS.Name)
 		})
 
-		By("check if cluster resource placement is updated to Applied", func() {
+		By("check if cluster resource placement is updated to Scheduled & Applied", func() {
+			framework.WaitConditionClusterResourcePlacement(*HubCluster, crp, string(v1alpha1.ResourcePlacementConditionTypeScheduled), v1.ConditionTrue, 3*framework.PollTimeout)
 			framework.WaitConditionClusterResourcePlacement(*HubCluster, crp, string(v1alpha1.ResourcePlacementStatusConditionTypeApplied), v1.ConditionTrue, 3*framework.PollTimeout)
 		})
+	})
 
-		//By("clean up", func() {
-		//	framework.DeleteMemberCluster(*HubCluster, mc)
-		//	Eventually(func() bool {
-		//		err := HubCluster.KubeClient.Get(context.TODO(), types.NamespacedName{Name: memberNS.Name, Namespace: ""}, memberNS)
-		//		return apierrors.IsNotFound(err)
-		//	}, framework.PollTimeout, framework.PollInterval).Should(Equal(true))
-		//	framework.DeleteNamespace(*MemberCluster, memberNS)
-		//	Eventually(func() bool {
-		//		err := MemberCluster.KubeClient.Get(context.TODO(), types.NamespacedName{Name: memberNS.Name, Namespace: ""}, memberNS)
-		//		return apierrors.IsNotFound(err)
-		//	}, framework.PollTimeout, framework.PollInterval).Should(Equal(true))
-		//})
-
+	AfterEach(func() {
+		framework.DeleteWork(*HubCluster, work)
+		Eventually(func() bool {
+			err := HubCluster.KubeClient.Get(context.TODO(), types.NamespacedName{Name: work.Name, Namespace: work.Namespace}, work)
+			return apierrors.IsNotFound(err)
+		}, framework.PollTimeout, framework.PollInterval).Should(Equal(true))
+		framework.DeleteMemberCluster(*HubCluster, mc)
+		Eventually(func() bool {
+			err := HubCluster.KubeClient.Get(context.TODO(), types.NamespacedName{Name: memberNS.Name, Namespace: ""}, memberNS)
+			return apierrors.IsNotFound(err)
+		}, framework.PollTimeout, framework.PollInterval).Should(Equal(true))
+		framework.DeleteNamespace(*MemberCluster, memberNS)
+		Eventually(func() bool {
+			err := MemberCluster.KubeClient.Get(context.TODO(), types.NamespacedName{Name: memberNS.Name, Namespace: ""}, memberNS)
+			return apierrors.IsNotFound(err)
+		}, framework.PollTimeout, framework.PollInterval).Should(Equal(true))
 	})
 })
