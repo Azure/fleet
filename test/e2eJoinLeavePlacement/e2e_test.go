d6c0ad3c2d7c2337aca2f365a4bc6d779cad36d4
Arvind Thirumurugan <arvindthirumurugan@gmail.com>
2022-09-27 06:34:55 +0530

    Working e2e suite

--- test/e2eJoinLeavePlacement/e2e_test.go
+++ test/e2eJoinLeavePlacement/e2e_test.go
@@ -1,34 +1,31 @@
-/*
-Copyright (c) Microsoft Corporation.
-Licensed under the MIT license.
-*/
-
 package e2eJoinLeavePlacement
 
 import (
 	"context"
-	. "github.com/onsi/ginkgo"
+	"os"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	"os"
-	"testing"
+	workv1alpha1 "sigs.k8s.io/work-api/pkg/apis/v1alpha1"
 
 	"go.goms.io/fleet/apis/v1alpha1"
 	"go.goms.io/fleet/test/e2e/framework"
 )
 
 var (
+	ctx               context.Context
+	scheme            = runtime.NewScheme()
+	hubURL            string
 	hubClusterName    = "kind-hub-testing"
 	memberClusterName = "kind-member-testing"
 	HubCluster        = framework.NewCluster(hubClusterName, scheme)
 	MemberCluster     = framework.NewCluster(memberClusterName, scheme)
-	hubURL            string
-	scheme            = runtime.NewScheme()
-	ctx               context.Context
 
 	imcJoinedAgentStatus = []v1alpha1.AgentStatus{
 		{
@@ -95,15 +92,17 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 	utilruntime.Must(v1alpha1.AddToScheme(scheme))
+	utilruntime.Must(workv1alpha1.AddToScheme(scheme))
 	utilruntime.Must(apiextensionsv1.AddToScheme(scheme))
 }
 
 func TestE2E(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "fleet e2e workload suite")
+	RunSpecs(t, "fleet join leave placement e2e suite")
 }
 
 var _ = BeforeSuite(func() {
+	ctx = context.Background()
 	kubeconfig := os.Getenv("KUBECONFIG")
 	Expect(kubeconfig).ShouldNot(BeEmpty(), "Failure to retrieve kubeconfig")
 	hubURL = os.Getenv("HUB_SERVER_URL")
