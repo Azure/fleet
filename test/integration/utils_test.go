b9c5460789d9b81219d87903835496e23fe29e6c
Arvind Thirumurugan <arvindthirumurugan@gmail.com>
2022-10-26 15:10:50 -0700

    revert change

--- test/integration/utils_test.go
+++ test/integration/utils_test.go
@@ -81,7 +81,7 @@ func GetObjectFromManifest(relativeFilePath string, obj runtime.Object) {
 // applyTestManifests creates the test manifests in the hub cluster.
 // Here is the list, please do NOT change this list unless you know what you are doing.
 // ClusterScoped resource:
-// Cloneset CRD, ClusterRole, Namespace.
+// Cloneset CRD, ClusterRole, Namespace
 // Namespaced resources:
 // Cloneset CR, Pdb, Configmap, Secret, Service.
 func applyTestManifests() {
