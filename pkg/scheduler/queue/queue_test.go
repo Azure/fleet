dc1bebacffa6151f5467b38e903d37baf02945c1
michaelawyu <chenyu1@microsoft.com>
2023-06-02 08:10:45 +0800

    Added scheduler queue

---
+++ pkg/scheduler/queue/queue_test.go
@@ -0,0 +1,37 @@
+/*
+Copyright (c) Microsoft Corporation.
+Licensed under the MIT license.
+*/
+
+package schedulingqueue
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+// TestSimplePolicySnapshotKeySchedulingQueueBasicOps tests the basic ops
+// (Add, NextClusterPolicySnapshotKey, Done) of a simplePolicySnapshotKeySchedulingQueue.
+func TestSimplePolicySnapshotKeySchedulingQueueBasicOps(t *testing.T) {
+	sq := NewSimplePolicySnapshotKeySchedulingQueue()
+	sq.Run()
+
+	keysToAdd := []PolicySnapshotKey{"A", "B", "C", "D", "E"}
+	for _, key := range keysToAdd {
+		sq.Add(key)
+	}
+
+	keysRecved := []PolicySnapshotKey{}
+	for i := 0; i < len(keysToAdd); i++ {
+		key := sq.NextClusterPolicySnapshotKey()
+		keysRecved = append(keysRecved, key)
+		sq.Done(key)
+	}
+
+	if !cmp.Equal(keysToAdd, keysRecved) {
+		t.Fatalf("Received keys %v, want %v", keysRecved, keysToAdd)
+	}
+
+	sq.Close()
+}
