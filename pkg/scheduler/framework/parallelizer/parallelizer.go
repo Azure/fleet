1fc57e54c13b93a3f1deeb79a3c677b6a1f6dc94
michaelawyu <chenyu1@microsoft.com>
2023-06-12 14:32:52 +0800

    Minor fixes

--- pkg/scheduler/framework/parallelizer/parallelizer.go
+++ pkg/scheduler/framework/parallelizer/parallelizer.go
@@ -3,7 +3,7 @@ Copyright (c) Microsoft Corporation.
 Licensed under the MIT license.
 */
 
-// package parallelizer features some utilities to help run tasks in parallel.
+// Package parallelizer features some utilities to help run tasks in parallel.
 package parallelizer
 
 import (
