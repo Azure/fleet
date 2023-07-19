d5a3ee3588e5a78f7c2f9b01c0aaf0af360ede1f
Zhiying Lin <zhiyingl456@gmail.com>
2023-07-19 17:18:41 +0800

    feat: add clusteraffinity plugin implementations

--- pkg/scheduler/framework/plugins/clusteraffinity/filtering.go
+++ pkg/scheduler/framework/plugins/clusteraffinity/filtering.go
@@ -2,9 +2,14 @@ package clusteraffinity
 
 import (
 	"context"
+	"fmt"
 
 	fleetv1beta1 "go.goms.io/fleet/apis/placement/v1beta1"
 	"go.goms.io/fleet/pkg/scheduler/framework"
+)
+
+const (
+	requiredAffinityViolationReasonTemplate = "none of the nonempty required cluster affinity term (total number: %d) is matched"
 )
 
 // PreFilter allows the plugin to connect to the PreFilter extension point in the scheduling
@@ -13,21 +18,64 @@ import (
 // Note that the scheduler will not run this extension point in parallel.
 func (p *Plugin) PreFilter(
 	_ context.Context,
-	_ framework.CycleStatePluginReadWriter,
+	state framework.CycleStatePluginReadWriter,
 	policy *fleetv1beta1.ClusterSchedulingPolicySnapshot,
 ) (status *framework.Status) {
-	// TODO not implemented yet
-	_, _ = preparePluginState(policy)
+	noClusterAffinity := policy.Spec.Policy == nil ||
+		policy.Spec.Policy.Affinity == nil ||
+		policy.Spec.Policy.Affinity.ClusterAffinity == nil
+
+	if noClusterAffinity {
+		// There are no clusterAffinity filter to enforce; skip.
+		//
+		// Note that this will lead the scheduler to skip this plugin in the next stage
+		// (Filter).
+		return framework.NewNonErrorStatus(framework.Skip, p.Name(), "no cluster affinity term is present")
+	}
+
+	// Prepare some common states for future use. This helps avoid the cost of repeatedly
+	// calculating the same states at each extension point.
+	ps, err := preparePluginState(policy)
+	if err != nil {
+		return framework.FromError(err, p.Name(), "failed to prepare plugin state")
+	}
+
+	// Save the plugin state.
+	state.Write(framework.StateKey(p.Name()), ps)
+
+	if len(ps.requiredAffinityTerms) == 0 {
+		// There are no required affinity terms to enforce; skip.
+		//
+		// Note that this will lead the scheduler to skip this plugin in the next stage
+		// (Filter).
+		return framework.NewNonErrorStatus(framework.Skip, p.Name(), "no required cluster affinity term is present or all of the terms are empty")
+	}
+
+	// All done.
 	return nil
 }
 
 // Filter allows the plugin to connect to the Filter extension point in the scheduling framework.
 func (p *Plugin) Filter(
 	_ context.Context,
-	_ framework.CycleStatePluginReadWriter,
+	state framework.CycleStatePluginReadWriter,
 	_ *fleetv1beta1.ClusterSchedulingPolicySnapshot,
-	_ *fleetv1beta1.MemberCluster,
+	cluster *fleetv1beta1.MemberCluster,
 ) (status *framework.Status) {
-	// TODO not implemented yet
-	return nil
+	// Read the plugin state.
+	ps, err := p.readPluginState(state)
+	if err != nil {
+		// This branch should never be reached, as for any policy with present required cluster affinity terms, a common
+		// plugin state has been set at the PreFilter extension point.
+		return framework.FromError(err, p.Name(), "failed to read plugin state")
+	}
+
+	if ps.requiredAffinityTerms.Matches(cluster) {
+		// all done.
+		return nil
+	}
+	// If the requiredAffinityTerms is 0, the prefilter should skip the filter state.
+	// When it reaches to this step, it means the cluster does not match any requiredAffinityTerm.
+	reason := fmt.Sprintf(requiredAffinityViolationReasonTemplate, len(ps.requiredAffinityTerms))
+	return framework.NewNonErrorStatus(framework.ClusterUnschedulable, p.Name(), reason)
 }
---
+++ pkg/scheduler/framework/plugins/clusteraffinity/filtering_test.go
@@ -0,0 +1,343 @@
+package clusteraffinity
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+
+	fleetv1beta1 "go.goms.io/fleet/apis/placement/v1beta1"
+	"go.goms.io/fleet/pkg/scheduler/framework"
+)
+
+var (
+	cmpStatusOptions = cmp.Options{
+		cmpopts.IgnoreFields(framework.Status{}, "reasons", "err"),
+		cmp.AllowUnexported(framework.Status{}),
+	}
+	cmpPluginStateOptions = cmp.Options{
+		cmp.AllowUnexported(pluginState{}, affinityTerm{}, preferredAffinityTerm{}),
+	}
+)
+
+func TestPreFilter(t *testing.T) {
+	tests := []struct {
+		name            string
+		policy          *fleetv1beta1.PlacementPolicy
+		want            *framework.Status
+		wantPluginState *pluginState
+	}{
+		{
+			name: "nil policy",
+			want: framework.NewNonErrorStatus(framework.Skip, defaultClusterAffinityPluginOptions.name),
+		},
+		{
+			name:   "nil affinity",
+			policy: &fleetv1beta1.PlacementPolicy{},
+			want:   framework.NewNonErrorStatus(framework.Skip, defaultClusterAffinityPluginOptions.name),
+		},
+		{
+			name: "nil cluster affinity",
+			policy: &fleetv1beta1.PlacementPolicy{
+				Affinity: &fleetv1beta1.Affinity{},
+			},
+			want: framework.NewNonErrorStatus(framework.Skip, defaultClusterAffinityPluginOptions.name),
+		},
+		{
+			name: "no cluster affinity",
+			policy: &fleetv1beta1.PlacementPolicy{
+				Affinity: &fleetv1beta1.Affinity{
+					ClusterAffinity: &fleetv1beta1.ClusterAffinity{},
+				},
+			},
+			want: framework.NewNonErrorStatus(framework.Skip, defaultClusterAffinityPluginOptions.name),
+			wantPluginState: &pluginState{
+				requiredAffinityTerms:  []affinityTerm{},
+				preferredAffinityTerms: []preferredAffinityTerm{},
+			},
+		},
+		{
+			name: "no required terms and empty preferred terms",
+			policy: &fleetv1beta1.PlacementPolicy{
+				Affinity: &fleetv1beta1.Affinity{
+					ClusterAffinity: &fleetv1beta1.ClusterAffinity{
+						RequiredDuringSchedulingIgnoredDuringExecution: &fleetv1beta1.ClusterSelector{},
+						PreferredDuringSchedulingIgnoredDuringExecution: []fleetv1beta1.PreferredClusterSelector{
+							{
+								Weight: 0,
+								Preference: fleetv1beta1.ClusterSelectorTerm{
+									LabelSelector: metav1.LabelSelector{
+										MatchLabels: map[string]string{
+											"region": "us-west",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: framework.NewNonErrorStatus(framework.Skip, defaultClusterAffinityPluginOptions.name),
+			wantPluginState: &pluginState{
+				requiredAffinityTerms:  []affinityTerm{},
+				preferredAffinityTerms: []preferredAffinityTerm{},
+			},
+		},
+		{
+			name: "no required terms and multiple preferred terms",
+			policy: &fleetv1beta1.PlacementPolicy{
+				Affinity: &fleetv1beta1.Affinity{
+					ClusterAffinity: &fleetv1beta1.ClusterAffinity{
+						RequiredDuringSchedulingIgnoredDuringExecution: &fleetv1beta1.ClusterSelector{},
+						PreferredDuringSchedulingIgnoredDuringExecution: []fleetv1beta1.PreferredClusterSelector{
+							{
+								Weight: 5,
+								Preference: fleetv1beta1.ClusterSelectorTerm{
+									LabelSelector: metav1.LabelSelector{
+										MatchLabels: map[string]string{
+											"region": "us-west",
+										},
+									},
+								},
+							},
+							{
+								Weight: 1,
+								Preference: fleetv1beta1.ClusterSelectorTerm{
+									LabelSelector: metav1.LabelSelector{
+										MatchLabels: map[string]string{},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: framework.NewNonErrorStatus(framework.Skip, defaultClusterAffinityPluginOptions.name),
+			wantPluginState: &pluginState{
+				requiredAffinityTerms: []affinityTerm{},
+				preferredAffinityTerms: []preferredAffinityTerm{
+					{
+						weight: 5,
+						affinityTerm: affinityTerm{
+							selector: labels.SelectorFromSet(map[string]string{"region": "us-west"}),
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "empty required terms and no preferred terms",
+			policy: &fleetv1beta1.PlacementPolicy{
+				Affinity: &fleetv1beta1.Affinity{
+					ClusterAffinity: &fleetv1beta1.ClusterAffinity{
+						RequiredDuringSchedulingIgnoredDuringExecution: &fleetv1beta1.ClusterSelector{
+							ClusterSelectorTerms: []fleetv1beta1.ClusterSelectorTerm{
+								{
+									LabelSelector: metav1.LabelSelector{
+										MatchLabels: map[string]string{},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: framework.NewNonErrorStatus(framework.Skip, defaultClusterAffinityPluginOptions.name),
+			wantPluginState: &pluginState{
+				requiredAffinityTerms:  []affinityTerm{},
+				preferredAffinityTerms: []preferredAffinityTerm{},
+			},
+		},
+		{
+			name: "multiple required terms and no preferred terms",
+			policy: &fleetv1beta1.PlacementPolicy{
+				Affinity: &fleetv1beta1.Affinity{
+					ClusterAffinity: &fleetv1beta1.ClusterAffinity{
+						RequiredDuringSchedulingIgnoredDuringExecution: &fleetv1beta1.ClusterSelector{
+							ClusterSelectorTerms: []fleetv1beta1.ClusterSelectorTerm{
+								{
+									LabelSelector: metav1.LabelSelector{
+										MatchLabels: map[string]string{"region": "us-west"},
+									},
+								},
+								{
+									LabelSelector: metav1.LabelSelector{},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: nil, // not skip the filter stage
+			wantPluginState: &pluginState{
+				requiredAffinityTerms: []affinityTerm{
+					{
+						selector: labels.SelectorFromSet(map[string]string{"region": "us-west"}),
+					},
+				},
+				preferredAffinityTerms: []preferredAffinityTerm{},
+			},
+		},
+		{
+			name: "multiple required terms and preferred terms",
+			policy: &fleetv1beta1.PlacementPolicy{
+				Affinity: &fleetv1beta1.Affinity{
+					ClusterAffinity: &fleetv1beta1.ClusterAffinity{
+						RequiredDuringSchedulingIgnoredDuringExecution: &fleetv1beta1.ClusterSelector{
+							ClusterSelectorTerms: []fleetv1beta1.ClusterSelectorTerm{
+								{
+									LabelSelector: metav1.LabelSelector{
+										MatchLabels: map[string]string{"region": "us-west"},
+									},
+								},
+							},
+						},
+						PreferredDuringSchedulingIgnoredDuringExecution: []fleetv1beta1.PreferredClusterSelector{
+							{
+								Weight: 5,
+								Preference: fleetv1beta1.ClusterSelectorTerm{
+									LabelSelector: metav1.LabelSelector{
+										MatchLabels: map[string]string{
+											"region": "us-west",
+										},
+									},
+								},
+							},
+							{
+								Weight: 1,
+								Preference: fleetv1beta1.ClusterSelectorTerm{
+									LabelSelector: metav1.LabelSelector{
+										MatchLabels: map[string]string{},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: nil, // not skip the filter stage
+			wantPluginState: &pluginState{
+				requiredAffinityTerms: []affinityTerm{
+					{
+						selector: labels.SelectorFromSet(map[string]string{"region": "us-west"}),
+					},
+				},
+				preferredAffinityTerms: []preferredAffinityTerm{
+					{
+						weight: 5,
+						affinityTerm: affinityTerm{
+							selector: labels.SelectorFromSet(map[string]string{"region": "us-west"}),
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			state := framework.NewCycleState(nil)
+			snapshot := &fleetv1beta1.ClusterSchedulingPolicySnapshot{
+				Spec: fleetv1beta1.SchedulingPolicySnapshotSpec{
+					Policy: tc.policy,
+				},
+			}
+			p := New()
+			got := p.PreFilter(context.Background(), state, snapshot)
+			if diff := cmp.Diff(tc.want, got, cmpStatusOptions); diff != "" {
+				t.Errorf("PreFilter() status mismatch (-want, +got):\n%s", diff)
+			}
+			if tc.wantPluginState == nil {
+				return
+			}
+			val, err := state.Read(framework.StateKey(p.Name()))
+			if err != nil {
+				t.Fatalf("Read() state got %v, want not nil", err)
+			}
+			gotPluginState, ok := val.(*pluginState)
+			if !ok {
+				t.Fatalf("failed to cast value %v to the pluginState type", val)
+			}
+			if diff := cmp.Diff(tc.wantPluginState, gotPluginState, cmpPluginStateOptions); diff != "" {
+				t.Errorf("Read() pluginState mismatch (-want, +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestFilter(t *testing.T) {
+	tests := []struct {
+		name              string
+		ps                *pluginState
+		notSetPluginState bool
+		cluster           *fleetv1beta1.MemberCluster
+		want              *framework.Status
+	}{
+		{
+			name:              "pluginState is not set",
+			notSetPluginState: true,
+			want:              framework.FromError(errors.New("invalid state"), defaultClusterAffinityPluginOptions.name),
+		},
+		{
+			name: "nil pluginState",
+			want: framework.FromError(errors.New("invalid state"), defaultClusterAffinityPluginOptions.name),
+		},
+		{
+			name: "matched cluster",
+			ps: &pluginState{
+				requiredAffinityTerms: []affinityTerm{
+					{
+						selector: labels.SelectorFromSet(map[string]string{"region": "us-west"}),
+					},
+				},
+			},
+			cluster: &fleetv1beta1.MemberCluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: clusterName,
+					Labels: map[string]string{
+						"region": "us-west",
+						"zone":   "zone2",
+					},
+				},
+			},
+			want: nil,
+		},
+		{
+			name: "not matched cluster",
+			ps: &pluginState{
+				requiredAffinityTerms: []affinityTerm{
+					{
+						selector: labels.SelectorFromSet(map[string]string{"region": "us-west"}),
+					},
+				},
+			},
+			cluster: &fleetv1beta1.MemberCluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: clusterName,
+					Labels: map[string]string{
+						"region": "us-east",
+						"zone":   "zone2",
+					},
+				},
+			},
+			want: framework.NewNonErrorStatus(framework.ClusterUnschedulable, defaultClusterAffinityPluginOptions.name),
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			p := New()
+			state := framework.NewCycleState(nil)
+			if !tc.notSetPluginState {
+				state.Write(framework.StateKey(p.Name()), tc.ps)
+			}
+
+			got := p.Filter(context.Background(), state, nil, tc.cluster)
+			if diff := cmp.Diff(tc.want, got, cmpStatusOptions); diff != "" {
+				t.Errorf("Filter() status mismatch (-want, +got):\n%s", diff)
+			}
+		})
+	}
+}
