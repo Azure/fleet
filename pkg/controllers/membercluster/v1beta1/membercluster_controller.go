7955ed1ce7b5ac328c272c7a16a359c04ad2d84b
michaelawyu <chenyu1@microsoft.com>
2023-09-19 17:40:31 +0800

    Fixes

--- pkg/controllers/membercluster/v1beta1/membercluster_controller.go
+++ pkg/controllers/membercluster/v1beta1/membercluster_controller.go
@@ -299,7 +299,7 @@ func (r *Reconciler) syncRole(ctx context.Context, mc *clusterv1beta1.MemberClus
 			Namespace:       namespaceName,
 			OwnerReferences: []metav1.OwnerReference{*toOwnerReference(mc)},
 		},
-		Rules: []rbacv1.PolicyRule{utils.FleetRule, utils.EventRule, utils.FleetNetworkRule, utils.WorkRule},
+		Rules: []rbacv1.PolicyRule{utils.FleetClusterRule, utils.FleetPlacementRule, utils.FleetNetworkRule, utils.EventRule},
 	}
 
 	// Creates role if not found.
