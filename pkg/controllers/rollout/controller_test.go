61f7937a4f969ee7f4b2847e4452ce0f8c3c9a0d
Britania Rodriguez Reyes <145056127+britaniar@users.noreply.github.com>
2023-11-14 14:28:44 -0800

    Fix format

--- pkg/controllers/rollout/controller_test.go
+++ pkg/controllers/rollout/controller_test.go
@@ -8,15 +8,15 @@ package rollout
 import (
 	"context"
 	"errors"
-	"github.com/crossplane/crossplane-runtime/pkg/test"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"reflect"
 	"testing"
 	"time"
 
+	"github.com/crossplane/crossplane-runtime/pkg/test"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/client-go/util/workqueue"
 	"k8s.io/utils/pointer"
