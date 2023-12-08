e5a1d81aad1f8f5ff1906f5c7021a125beeabb9f
Arvind Thirumurugan <arvindthirumurugan@gmail.com>
2023-12-07 18:26:40 -0800

    revert API doc change

--- apis/placement/v1beta1/binding_types.go
+++ apis/placement/v1beta1/binding_types.go
@@ -18,7 +18,7 @@ import (
 // +kubebuilder:printcolumn:JSONPath=`.metadata.creationTimestamp`,name="Age",type=date
 
 // ClusterResourceBinding represents a scheduling decision that binds a group of resources to a cluster.
-// It MUST have a label named `parent-CRP` that points to the cluster resource policy that creates it.
+// It MUST have a label named `CRPTrackingLabel` that points to the cluster resource policy that creates it.
 type ClusterResourceBinding struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
