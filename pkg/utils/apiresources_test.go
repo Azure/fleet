9a7abbba45fc2b9c3f2f0eae613f7264979427d1
Vasudev Bongale <vbongale@linkedin.com>
2023-12-19 16:44:07 +0530

    Add tests for default resource configs

--- pkg/utils/apiresources_test.go
+++ pkg/utils/apiresources_test.go
@@ -229,162 +229,172 @@ func TestResourceConfigGroupParse(t *testing.T) {
 }
 
 func TestResourceConfigMixedParse(t *testing.T) {
-	tests := []struct {
-		input    string
-		disabled []schema.GroupVersionKind
-		enabled  []schema.GroupVersionKind
-	}{
+	input := "v1/Node,Pod;networking.k8s.io;apps/v1;authorization.k8s.io/v1/SelfSubjectRulesReview"
+
+	// these are the resources that are in the scope of the user specified input
+	resourcesInUserInput := []schema.GroupVersionKind{
 		{
-			input: "v1/Node,Pod;networking.k8s.io;apps/v1;authorization.k8s.io/v1/SelfSubjectRulesReview",
-			disabled: []schema.GroupVersionKind{
-				{
-					Group:   "networking.k8s.io",
-					Version: "v1beta1",
-					Kind:    "Ingress",
-				},
-				{
-					Group:   "networking.k8s.io",
-					Version: "v1",
-					Kind:    "IngressClass",
-				},
-				{
-					Group:   "",
-					Version: "v1",
-					Kind:    "Node",
-				},
-				{
-					Group:   "",
-					Version: "v1",
-					Kind:    "Pod",
-				},
-				{
-					Group:   "authorization.k8s.io",
-					Version: "v1",
-					Kind:    "SelfSubjectRulesReview",
-				},
-				{
-					Group:   "apps",
-					Version: "v1",
-					Kind:    "HealthState",
-				},
-			},
-			enabled: []schema.GroupVersionKind{
-				{
-					Group:   "",
-					Version: "v1",
-					Kind:    "Ingress",
-				},
-				{
-					Group:   "apps",
-					Version: "v1beta1",
-					Kind:    "IngressClass",
-				},
-				{
-					Group:   "authorization.k8s.io",
-					Version: "v1",
-					Kind:    "LocalSubjectAccessReview",
-				},
-			},
+			Group:   "networking.k8s.io",
+			Version: "v1beta1",
+			Kind:    "Ingress",
+		},
+		{
+			Group:   "networking.k8s.io",
+			Version: "v1",
+			Kind:    "IngressClass",
+		},
+		{
+			Group:   "",
+			Version: "v1",
+			Kind:    "Node",
+		},
+		{
+			Group:   "",
+			Version: "v1",
+			Kind:    "Pod",
+		},
+		{
+			Group:   "authorization.k8s.io",
+			Version: "v1",
+			Kind:    "SelfSubjectRulesReview",
+		},
+		{
+			Group:   "apps",
+			Version: "v1",
+			Kind:    "HealthState",
 		},
 	}
-	for _, test := range tests {
-		r := NewResourceConfig(false)
-		if err := r.Parse(test.input); err != nil {
-			t.Fatalf("Unexpected error: %v", err)
-		}
-		for i, o := range test.disabled {
-			ok := r.IsResourceDisabled(o)
-			if !ok {
-				t.Errorf("%d: unexpected error: %v", i, o)
-			}
-		}
-		for i, o := range test.enabled {
-			ok := r.IsResourceDisabled(o)
-			if ok {
-				t.Errorf("%d: unexpected error: %v", i, o)
-			}
-		}
+
+	// these are the resources that are not in the scope of the user specified input
+	resourcesNotInUserInput := []schema.GroupVersionKind{
+		{
+			Group:   "",
+			Version: "v1",
+			Kind:    "Ingress",
+		},
+		{
+			Group:   "apps",
+			Version: "v1beta1",
+			Kind:    "IngressClass",
+		},
+		{
+			Group:   "authorization.k8s.io",
+			Version: "v1",
+			Kind:    "LocalSubjectAccessReview",
+		},
 	}
-	for _, test := range tests {
-		// test allow list
-		r := NewResourceConfig(true)
-		if err := r.Parse(test.input); err != nil {
-			t.Fatalf("Parse() returned error: %v", err)
-		}
-		// Since we are testing allow list, the result is reversed
-		for i, o := range test.disabled {
-			if ok := r.IsResourceDisabled(o); ok {
-				t.Errorf("%d: expected resource to be enabled : %v", i, o)
+
+	tests := map[string]struct {
+		isAllowList bool
+		disabled    []schema.GroupVersionKind
+		enabled     []schema.GroupVersionKind
+	}{
+		"disabled list": {
+			isAllowList: false,
+			disabled:    resourcesInUserInput,
+			enabled:     resourcesNotInUserInput,
+		},
+		"enabled list": {
+			isAllowList: true,
+			disabled:    resourcesNotInUserInput,
+			enabled:     resourcesInUserInput,
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			r := NewResourceConfig(test.isAllowList)
+			if err := r.Parse(input); err != nil {
+				t.Fatalf("Parse() returned error: %v", err)
 			}
-		}
-		for i, o := range test.enabled {
-			if ok := r.IsResourceDisabled(o); !ok {
-				t.Errorf("%d: expected resource to be disabled : %v", i, o)
+
+			for i, o := range test.disabled {
+				if ok := r.IsResourceDisabled(o); !ok {
+					t.Errorf("%d: expected resource to be disabled : %v", i, o)
+				}
 			}
-		}
+
+			for i, o := range test.enabled {
+				if ok := r.IsResourceDisabled(o); ok {
+					t.Errorf("%d: expected resource to be enabled : %v", i, o)
+				}
+			}
+		})
 	}
 }
 
-func TestDefaultDisabledResourceConfigGroupVersionKindParse(t *testing.T) {
-	tests := []struct {
-		disabled []schema.GroupVersionKind
-		enabled  []schema.GroupVersionKind
-	}{
+func TestDefaultResourceConfigGroupVersionKindParse(t *testing.T) {
+
+	resourcesInDefaultDisabledList := []schema.GroupVersionKind{
+		corev1PodGVK, corev1NodeGVK,
 		{
-			disabled: []schema.GroupVersionKind{
-				corev1PodGVK, corev1NodeGVK,
-				{
-					Group:   "fleet.azure.com",
-					Version: "v1beta1",
-					Kind:    "MemberCluster",
-				},
-				{
-					Group:   "fleet.azure.com",
-					Version: "v1alpha1",
-					Kind:    "MemberCluster",
-				},
-				{
-					Group:   "events.k8s.io",
-					Version: "v1beta1",
-					Kind:    "Event",
-				},
-			},
-			enabled: []schema.GroupVersionKind{
-				{
-					Group:   "",
-					Version: "v1",
-					Kind:    "Namespace",
-				},
-				{
-					Group:   "apps",
-					Version: "v1",
-					Kind:    "Deployment",
-				},
-				{
-					Group:   "",
-					Version: "v1",
-					Kind:    "Event",
-				},
-			},
+			Group:   "fleet.azure.com",
+			Version: "v1beta1",
+			Kind:    "MemberCluster",
+		},
+		{
+			Group:   "fleet.azure.com",
+			Version: "v1alpha1",
+			Kind:    "MemberCluster",
+		},
+		{
+			Group:   "events.k8s.io",
+			Version: "v1beta1",
+			Kind:    "Event",
 		},
 	}
-	for _, test := range tests {
-		r := NewResourceConfig(false)
-		if err := r.Parse(""); err != nil {
-			t.Fatalf("Unexpected error: %v", err)
-		}
-		for i, o := range test.disabled {
-			ok := r.IsResourceDisabled(o)
-			if !ok {
-				t.Errorf("%d: unexpected error: %v", i, o)
+
+	resourcesNotInDefaultResourcesList := []schema.GroupVersionKind{
+		{
+			Group:   "",
+			Version: "v1",
+			Kind:    "Namespace",
+		},
+		{
+			Group:   "apps",
+			Version: "v1",
+			Kind:    "Deployment",
+		},
+		{
+			Group:   "",
+			Version: "v1",
+			Kind:    "Event",
+		},
+	}
+
+	tests := map[string]struct {
+		isAllowList bool
+		disabled    []schema.GroupVersionKind
+		enabled     []schema.GroupVersionKind
+	}{
+		"default disabled list": {
+			isAllowList: false,
+			disabled:    resourcesInDefaultDisabledList,
+			enabled:     resourcesNotInDefaultResourcesList,
+		},
+		"default enabled list": {
+			isAllowList: true,
+			disabled:    append(resourcesNotInDefaultResourcesList, resourcesInDefaultDisabledList...),
+			enabled:     []schema.GroupVersionKind{},
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			r := NewResourceConfig(test.isAllowList)
+			if err := r.Parse(""); err != nil {
+				t.Fatalf("Parse() returned error: %v", err)
 			}
-		}
-		for i, o := range test.enabled {
-			ok := r.IsResourceDisabled(o)
-			if ok {
-				t.Errorf("%d: unexpected error: %v", i, o)
+			for i, o := range test.disabled {
+				if ok := r.IsResourceDisabled(o); !ok {
+					t.Errorf("%d: expected resource to be disabled : %v", i, o)
+				}
 			}
-		}
+
+			for i, o := range test.enabled {
+				if ok := r.IsResourceDisabled(o); ok {
+					t.Errorf("%d: expected resource to be enabled : %v", i, o)
+				}
+			}
+		})
 	}
 }
 
