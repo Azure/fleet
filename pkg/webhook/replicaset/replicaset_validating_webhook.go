3914b938c455c1e63cc921f39b1a460960a81ee2
Arvind Thirumurugan <arvindthirumurugan@gmail.com>
2024-03-14 17:06:26 -0700

    add logs for pod, replica set validating webhooks (#722)

--- pkg/webhook/replicaset/replicaset_validating_webhook.go
+++ pkg/webhook/replicaset/replicaset_validating_webhook.go
@@ -12,11 +12,19 @@ import (
 
 	admissionv1 "k8s.io/api/admission/v1"
 	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/klog/v2"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
 	"go.goms.io/fleet/pkg/utils"
+)
+
+const (
+	deniedReplicaSetResource  = "ReplicaSet creation is disallowed in the fleet hub cluster"
+	allowedReplicaSetResource = "ReplicaSet creation is allowed in the fleet hub cluster"
+	replicaSetDeniedFormat    = "ReplicaSet %s/%s creation is disallowed in the fleet hub cluster."
 )
 
 var (
@@ -37,14 +45,18 @@ func Add(mgr manager.Manager) error {
 
 // Handle replicaSetValidator denies all creation requests.
 func (v *replicaSetValidator) Handle(_ context.Context, req admission.Request) admission.Response {
+	namespacedName := types.NamespacedName{Name: req.Name, Namespace: req.Namespace}
 	if req.Operation == admissionv1.Create {
+		klog.V(2).InfoS("handling replicaSet resource", "operation", req.Operation, "subResource", req.SubResource, "namespacedName", namespacedName)
 		rs := &appsv1.ReplicaSet{}
 		if err := v.decoder.Decode(req, rs); err != nil {
 			return admission.Errored(http.StatusBadRequest, err)
 		}
 		if !utils.IsReservedNamespace(rs.Namespace) {
-			return admission.Denied(fmt.Sprintf("ReplicaSet %s/%s creation is disallowed in the fleet hub cluster.", rs.Namespace, rs.Name))
+			klog.V(2).InfoS(deniedReplicaSetResource, "user", req.UserInfo.Username, "groups", req.UserInfo.Groups, "operation", req.Operation, "GVK", req.RequestKind, "subResource", req.SubResource, "namespacedName", namespacedName)
+			return admission.Denied(fmt.Sprintf(replicaSetDeniedFormat, rs.Namespace, rs.Name))
 		}
 	}
+	klog.V(3).InfoS(allowedReplicaSetResource, "user", req.UserInfo.Username, "groups", req.UserInfo.Groups, "operation", req.Operation, "GVK", req.RequestKind, "subResource", req.SubResource, "namespacedName", namespacedName)
 	return admission.Allowed("")
 }
