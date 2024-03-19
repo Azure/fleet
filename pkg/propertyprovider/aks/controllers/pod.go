fd503ffaa7007ad748cef648398ab90bf62ee441
michaelawyu <chenyu1@microsoft.com>
2024-03-19 22:30:52 +0800

    Added AKS property provider

---
+++ pkg/propertyprovider/aks/controllers/pod.go
@@ -0,0 +1,96 @@
+/*
+Copyright (c) Microsoft Corporation.
+Licensed under the MIT license.
+*/
+
+package controllers
+
+import (
+	"context"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/klog/v2"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"go.goms.io/fleet/pkg/propertyprovider/aks/trackers"
+)
+
+// TO-DO (chenyu1): this is a relatively expensive watcher, due to how frequent pods can change
+// in a Kubernetes cluster; unfortunately at this moment there does not seem to be a better way
+// to observe the changes of requested resources in a cluster. The alternative, which is to use
+// Lists, adds too much overhead to the API server.
+
+// PodReconciler reconciles Pod objects.
+type PodReconciler struct {
+	PT     *trackers.PodTracker
+	Client client.Client
+}
+
+// Reconcile reconciles a pod object.
+func (p *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	podRef := klog.KRef(req.Namespace, req.Name)
+	startTime := time.Now()
+	klog.V(2).InfoS("Reconciliation starts", "pod", podRef)
+	defer func() {
+		latency := time.Since(startTime).Milliseconds()
+		klog.V(2).InfoS("Reconciliation ends", "pod", podRef, "latency", latency)
+	}()
+
+	// Retrieve the pod object.
+	pod := &corev1.Pod{}
+	if err := p.Client.Get(ctx, req.NamespacedName, pod); err != nil {
+		// Failed to get the pod object.
+		if errors.IsNotFound(err) {
+			// This branch essentially processes the pod deletion event (the actual deletion).
+			// At this point the pod may have not been tracked by the tracker at all; if that's
+			// the case, the removal (untracking) operation is a no-op.
+			//
+			// Note that this controller will not add any finalizer to pod objects, so as to
+			// avoid blocking normal Kuberneters operations under unexpected circumstances.
+			p.PT.Remove(req.NamespacedName.String())
+			return ctrl.Result{}, nil
+		}
+
+		// For other errors, retry the reconciliation.
+		klog.V(2).ErrorS(err, "Failed to get the pod object", "pod", podRef)
+		return ctrl.Result{}, err
+	}
+
+	// Note that this controller will not untrack a pod when it is first marked for deletion;
+	// instead, it performs the untracking when the pod object is actually gone from the
+	// etcd store. This is intentional, as when a pod is marked for deletion, workloads might
+	// not have been successfully terminated yet, and untracking the pod too early might lead to a
+	// case of temporary inconsistency.
+
+	// Track the pod if:
+	//
+	// * it is **NOT** of the Succeeded or Failed state; and
+	// * it has been assigned to a node.
+	//
+	// This behavior is consistent with how the Kubernetes CLI tool reports requested capacity
+	// on a specific node (`kubectl describe node` command).
+	//
+	// Note that the tracker will attempt to track the pod even if it has been marked for deletion.
+	if len(pod.Spec.NodeName) > 0 && pod.Status.Phase != corev1.PodSucceeded && pod.Status.Phase != corev1.PodFailed {
+		klog.V(2).InfoS("Attempt to track the pod", "pod", podRef)
+		p.PT.AddOrUpdate(pod)
+	} else {
+		// Untrack the pod.
+		//
+		// It may have been descheduled, or transited into a terminal state.
+		klog.V(2).InfoS("Untrack the pod", "pod", podRef)
+		p.PT.Remove(req.NamespacedName.String())
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func (p *PodReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	// Reconcile any pod changes (create, update, delete).
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&corev1.Pod{}).
+		Complete(p)
+}
---
+++ pkg/propertyprovider/aks/provider_test.go
@@ -0,0 +1,357 @@
+/*
+Copyright (c) Microsoft Corporation.
+Licensed under the MIT license.
+*/
+
+package aks
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	clusterv1beta1 "go.goms.io/fleet/apis/cluster/v1beta1"
+	"go.goms.io/fleet/pkg/propertyprovider"
+	"go.goms.io/fleet/pkg/propertyprovider/aks/trackers"
+)
+
+const (
+	nodeName1 = "node-1"
+	nodeName2 = "node-2"
+	nodeName3 = "node-3"
+	nodeName4 = "node-4"
+
+	podName1 = "pod-1"
+	podName2 = "pod-2"
+	podName3 = "pod-3"
+	podName4 = "pod-4"
+
+	containerName1 = "container-1"
+	containerName2 = "container-2"
+	containerName3 = "container-3"
+	containerName4 = "container-4"
+
+	namespaceName1 = "work-1"
+	namespaceName2 = "work-2"
+	namespaceName3 = "work-3"
+
+	nodeSKU1 = "Standard_1"
+	nodeSKU2 = "Standard_2"
+
+	imageName = "nginx"
+)
+
+var (
+	currentTime = time.Now()
+)
+
+// dummyPricingProvider is a mock implementation that implements the PricingProvider interface.
+type dummyPricingProvider struct{}
+
+var _ trackers.PricingProvider = &dummyPricingProvider{}
+
+func (d *dummyPricingProvider) OnDemandPrice(_ string) (float64, bool) {
+	return 1.0, true
+}
+
+func (d *dummyPricingProvider) LastUpdated() time.Time {
+	return currentTime
+}
+
+func TestCollect(t *testing.T) {
+	testCases := []struct {
+		name                         string
+		nodes                        []corev1.Node
+		pods                         []corev1.Pod
+		wantMetricCollectionResponse propertyprovider.PropertyCollectionResponse
+	}{
+		{
+			name: "can report properties",
+			nodes: []corev1.Node{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: nodeName1,
+						Labels: map[string]string{
+							trackers.AKSClusterNodeSKULabelName: nodeSKU1,
+						},
+					},
+					Spec: corev1.NodeSpec{},
+					Status: corev1.NodeStatus{
+						Capacity: corev1.ResourceList{
+							corev1.ResourceCPU:    resource.MustParse("4"),
+							corev1.ResourceMemory: resource.MustParse("16Gi"),
+						},
+						Allocatable: corev1.ResourceList{
+							corev1.ResourceCPU:    resource.MustParse("3.2"),
+							corev1.ResourceMemory: resource.MustParse("15.2Gi"),
+						},
+					},
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: nodeName2,
+						Labels: map[string]string{
+							trackers.AKSClusterNodeSKULabelName: nodeSKU2,
+						},
+					},
+					Spec: corev1.NodeSpec{},
+					Status: corev1.NodeStatus{
+						Capacity: corev1.ResourceList{
+							corev1.ResourceCPU:    resource.MustParse("8"),
+							corev1.ResourceMemory: resource.MustParse("32Gi"),
+						},
+						Allocatable: corev1.ResourceList{
+							corev1.ResourceCPU:    resource.MustParse("7"),
+							corev1.ResourceMemory: resource.MustParse("30Gi"),
+						},
+					},
+				},
+			},
+			pods: []corev1.Pod{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      podName1,
+						Namespace: namespaceName1,
+					},
+					Spec: corev1.PodSpec{
+						NodeName: nodeName1,
+						Containers: []corev1.Container{
+							{
+								Name: containerName1,
+								Resources: corev1.ResourceRequirements{
+									Requests: corev1.ResourceList{
+										corev1.ResourceCPU:    resource.MustParse("1"),
+										corev1.ResourceMemory: resource.MustParse("2Gi"),
+									},
+								},
+							},
+							{
+								Name: containerName2,
+								Resources: corev1.ResourceRequirements{
+									Requests: corev1.ResourceList{
+										corev1.ResourceCPU:    resource.MustParse("1.5"),
+										corev1.ResourceMemory: resource.MustParse("4Gi"),
+									},
+								},
+							},
+						},
+					},
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      podName2,
+						Namespace: namespaceName1,
+					},
+					Spec: corev1.PodSpec{
+						NodeName: nodeName2,
+						Containers: []corev1.Container{
+							{
+								Name: containerName3,
+								Resources: corev1.ResourceRequirements{
+									Requests: corev1.ResourceList{
+										corev1.ResourceCPU:    resource.MustParse("6"),
+										corev1.ResourceMemory: resource.MustParse("16Gi"),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			wantMetricCollectionResponse: propertyprovider.PropertyCollectionResponse{
+				Properties: map[clusterv1beta1.PropertyName]clusterv1beta1.PropertyValue{
+					NodeCountProperty: {
+						Value: "2",
+					},
+					PerCPUCoreCostProperty: {
+						Value: "0.167",
+					},
+					PerGBMemoryCostProperty: {
+						Value: "0.042",
+					},
+				},
+				Resources: clusterv1beta1.ResourceUsage{
+					Capacity: corev1.ResourceList{
+						corev1.ResourceCPU:    resource.MustParse("12"),
+						corev1.ResourceMemory: resource.MustParse("48Gi"),
+					},
+					Allocatable: corev1.ResourceList{
+						corev1.ResourceCPU:    resource.MustParse("10.2"),
+						corev1.ResourceMemory: resource.MustParse("45.2Gi"),
+					},
+					Available: corev1.ResourceList{
+						corev1.ResourceCPU:    resource.MustParse("1.7"),
+						corev1.ResourceMemory: resource.MustParse("23.2Gi"),
+					},
+				},
+				Conditions: []metav1.Condition{
+					{
+						Type:    PropertyCollectionSucceededConditionType,
+						Status:  metav1.ConditionTrue,
+						Reason:  PropertyCollectionSucceededReason,
+						Message: PropertyCollectionSucceededMessage,
+					},
+				},
+			},
+		},
+		{
+			name: "will report zero values if the requested resources exceed the allocatable resources",
+			nodes: []corev1.Node{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: nodeName1,
+						Labels: map[string]string{
+							trackers.AKSClusterNodeSKULabelName: nodeSKU1,
+						},
+					},
+					Spec: corev1.NodeSpec{},
+					Status: corev1.NodeStatus{
+						Capacity: corev1.ResourceList{
+							corev1.ResourceCPU:    resource.MustParse("4"),
+							corev1.ResourceMemory: resource.MustParse("16Gi"),
+						},
+						Allocatable: corev1.ResourceList{
+							corev1.ResourceCPU:    resource.MustParse("3.2"),
+							corev1.ResourceMemory: resource.MustParse("15.2Gi"),
+						},
+					},
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: nodeName2,
+						Labels: map[string]string{
+							trackers.AKSClusterNodeSKULabelName: nodeSKU2,
+						},
+					},
+					Spec: corev1.NodeSpec{},
+					Status: corev1.NodeStatus{
+						Capacity: corev1.ResourceList{
+							corev1.ResourceCPU:    resource.MustParse("8"),
+							corev1.ResourceMemory: resource.MustParse("32Gi"),
+						},
+						Allocatable: corev1.ResourceList{
+							corev1.ResourceCPU:    resource.MustParse("7"),
+							corev1.ResourceMemory: resource.MustParse("30Gi"),
+						},
+					},
+				},
+			},
+			pods: []corev1.Pod{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      podName1,
+						Namespace: namespaceName1,
+					},
+					Spec: corev1.PodSpec{
+						NodeName: nodeName1,
+						Containers: []corev1.Container{
+							{
+								Name: containerName1,
+								Resources: corev1.ResourceRequirements{
+									Requests: corev1.ResourceList{
+										corev1.ResourceCPU:    resource.MustParse("10"),
+										corev1.ResourceMemory: resource.MustParse("20Gi"),
+									},
+								},
+							},
+							{
+								Name: containerName2,
+								Resources: corev1.ResourceRequirements{
+									Requests: corev1.ResourceList{
+										corev1.ResourceCPU:    resource.MustParse("20"),
+										corev1.ResourceMemory: resource.MustParse("20Gi"),
+									},
+								},
+							},
+						},
+					},
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      podName2,
+						Namespace: namespaceName1,
+					},
+					Spec: corev1.PodSpec{
+						NodeName: nodeName2,
+						Containers: []corev1.Container{
+							{
+								Name: containerName3,
+								Resources: corev1.ResourceRequirements{
+									Requests: corev1.ResourceList{
+										corev1.ResourceCPU:    resource.MustParse("20"),
+										corev1.ResourceMemory: resource.MustParse("20Gi"),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			wantMetricCollectionResponse: propertyprovider.PropertyCollectionResponse{
+				Properties: map[clusterv1beta1.PropertyName]clusterv1beta1.PropertyValue{
+					NodeCountProperty: {
+						Value: "2",
+					},
+					PerCPUCoreCostProperty: {
+						Value: "0.167",
+					},
+					PerGBMemoryCostProperty: {
+						Value: "0.042",
+					},
+				},
+				Resources: clusterv1beta1.ResourceUsage{
+					Capacity: corev1.ResourceList{
+						corev1.ResourceCPU:    resource.MustParse("12"),
+						corev1.ResourceMemory: resource.MustParse("48Gi"),
+					},
+					Allocatable: corev1.ResourceList{
+						corev1.ResourceCPU:    resource.MustParse("10.2"),
+						corev1.ResourceMemory: resource.MustParse("45.2Gi"),
+					},
+					Available: corev1.ResourceList{
+						corev1.ResourceCPU:    resource.Quantity{},
+						corev1.ResourceMemory: resource.Quantity{},
+					},
+				},
+				Conditions: []metav1.Condition{
+					{
+						Type:    PropertyCollectionSucceededConditionType,
+						Status:  metav1.ConditionTrue,
+						Reason:  PropertyCollectionSucceededReason,
+						Message: PropertyCollectionSucceededMessage,
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctx := context.Background()
+
+			// Build the trackers manually for testing purposes.
+			nt := trackers.NewNodeTracker(&dummyPricingProvider{})
+			pt := trackers.NewPodTracker()
+			for idx := range tc.nodes {
+				nt.AddOrUpdate(&tc.nodes[idx])
+			}
+			for idx := range tc.pods {
+				pt.AddOrUpdate(&tc.pods[idx])
+			}
+
+			p := &PropertyProvider{
+				nt: nt,
+				pt: pt,
+			}
+			res := p.Collect(ctx)
+			if diff := cmp.Diff(res, tc.wantMetricCollectionResponse, ignoreObservationTimeFieldInPropertyValue); diff != "" {
+				t.Fatalf("Collect() property collection response diff (-got, +want):\n%s", diff)
+			}
+		})
+	}
+}
