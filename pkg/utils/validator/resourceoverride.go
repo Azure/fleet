24c74a31b1ae220d2f235c5c7ce2dece0ddc4ddd
Britania Rodriguez Reyes <britaniar@microsoft.com>
2024-03-26 12:21:50 -0700

    resource override validating functions

--- pkg/utils/validator/resourceoverride.go
+++ pkg/utils/validator/resourceoverride.go
@@ -1,64 +1,58 @@
+/*
+Copyright (c) Microsoft Corporation.
+Licensed under the MIT license.
+*/
+
+// Package validator provides utils to validate ResourceOverride resources.
 package validator
 
 import (
-	"context"
 	"fmt"
 
-	admissionv1 "k8s.io/api/admission/v1"
 	"k8s.io/apimachinery/pkg/util/errors"
-	"k8s.io/klog/v2"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	fleetv1alpha1 "go.goms.io/fleet/apis/placement/v1alpha1"
 )
 
 // ValidateResourceOverride validates resource override fields and returns error.
-func ValidateResourceOverride(ctx context.Context, client client.Client, operation admissionv1.Operation, ro fleetv1alpha1.ResourceOverride) error {
-	allErr := make([]error, 0)
-
+func ValidateResourceOverride(ro fleetv1alpha1.ResourceOverride, roList *fleetv1alpha1.ResourceOverrideList) error {
 	// Check if the resource is being selected by resource name
-	if err := validateResourceSelected(ro); err != nil {
+	if err := validateResourceSelectors(ro); err != nil {
+		// Skip the resource limit check because the check is only valid if resource is selected by name
 		return err
-	}
-	// List of resource overrides
-	roList, err := listResourceOverrideList(ctx, client)
-	if err != nil {
-		return err
-	}
-	// Check if the override count limit has been reached
-	if !ValidateResourceOverrideLimit(operation, roList) {
-		allErr = append(allErr, fmt.Errorf("at most 100 resource overrides can be created"))
 	}
 
 	// Check if the override count limit for the resources has been reached
-	if err = ValidateResourceOverrideResourceLimit(operation, ro, roList); err != nil {
-		allErr = append(allErr, err)
+	return validateResourceOverrideResourceLimit(ro, roList)
+}
+
+// validateResourceSelectors checks if override is selecting resource by name.
+func validateResourceSelectors(ro fleetv1alpha1.ResourceOverride) error {
+	selectorMap := make(map[fleetv1alpha1.ResourceSelector]bool)
+	allErr := make([]error, 0)
+	for _, selector := range ro.Spec.ResourceSelectors {
+		// Check if the resource is being selected by resource name
+		if selector.Name == "" {
+			allErr = append(allErr, fmt.Errorf("resource name is required for resource selection %+v", selector))
+			continue
+		}
+
+		// Check if there are any duplicate selectors
+		if selectorMap[selector] {
+			allErr = append(allErr, fmt.Errorf("resource selector %+v already exists, and must be unique", selector))
+		}
+		selectorMap[selector] = true
 	}
 	return errors.NewAggregate(allErr)
 }
 
-// validateResourceSelected checks if override is selecting resource by name.
-func validateResourceSelected(ro fleetv1alpha1.ResourceOverride) error {
-	for _, selector := range ro.Spec.ResourceSelectors {
-		if selector.Name == "" {
-			return fmt.Errorf("resource name is required for resource selection %+v", selector)
-		}
-	}
-	return nil
-}
-
-// ValidateResourceOverrideLimit checks if there are at most 100 cluster resource overrides.
-func ValidateResourceOverrideLimit(operation admissionv1.Operation, croList *fleetv1alpha1.ResourceOverrideList) bool {
-	// Check if the override count limit has been reached
-	if operation == admissionv1.Create {
-		return len(croList.Items) < 100
-	}
-	return len(croList.Items) <= 100
-}
-
-// ValidateResourceOverrideResourceLimit checks if there is only 1 cluster resource override per resource,
+// validateResourceOverrideResourceLimit checks if there is only 1 resource override per resource,
 // assuming the resource will be selected by the name only.
-func ValidateResourceOverrideResourceLimit(operation admissionv1.Operation, cro fleetv1alpha1.ResourceOverride, roList *fleetv1alpha1.ResourceOverrideList) error {
+func validateResourceOverrideResourceLimit(ro fleetv1alpha1.ResourceOverride, roList *fleetv1alpha1.ResourceOverrideList) error {
+	// Check if roList is nil or empty, no need to check for resource limit
+	if roList == nil || len(roList.Items) == 0 {
+		return nil
+	}
 	overrideMap := make(map[fleetv1alpha1.ResourceSelector]string)
 	// Add overrides and its selectors to the map
 	for _, override := range roList.Items {
@@ -68,25 +62,16 @@ func ValidateResourceOverrideResourceLimit(operation admissionv1.Operation, cro 
 		}
 	}
 
-	// Check if any of the cro selectors exist in the override map
-	for _, croSelector := range cro.Spec.ResourceSelectors {
-		if overrideMap[croSelector] != "" {
-			// If update operation, ignore the same cluster resource override
-			if cro.GetName() == overrideMap[croSelector] && operation == admissionv1.Update {
+	allErr := make([]error, 0)
+	// Check if any of the ro selectors exist in the override map
+	for _, roSelector := range ro.Spec.ResourceSelectors {
+		if overrideMap[roSelector] != "" {
+			// Ignore the same resource override
+			if ro.GetName() == overrideMap[roSelector] {
 				continue
 			}
-			return fmt.Errorf("the resource %v has been selected by both %v and %v, which are not supported", croSelector.Name, cro.GetName(), overrideMap[croSelector])
+			allErr = append(allErr, fmt.Errorf("invalid resource selector %+v: the resource has been selected by both %v and %v, which is not supported", roSelector, ro.GetName(), overrideMap[roSelector]))
 		}
 	}
-	return nil
-}
-
-// listResourceOverrideList returns a list of resource overrides.
-func listResourceOverrideList(ctx context.Context, client client.Client) (*fleetv1alpha1.ResourceOverrideList, error) {
-	roList := &fleetv1alpha1.ResourceOverrideList{}
-	if err := client.List(ctx, roList); err != nil {
-		klog.ErrorS(err, "Failed to list resource overrides when validating")
-		return nil, fmt.Errorf("failed to list resource overrides, please retry the request: %w", err)
-	}
-	return roList, nil
+	return errors.NewAggregate(allErr)
 }
