5032cd4d5e120bbe231b2ac515970216d6bb04a7
Britania Rodriguez Reyes <britaniar@microsoft.com>
2024-03-26 11:42:52 -0700

    address comments

--- pkg/webhook/clusterresourceoverride/clusterresourceoverride_validating_webhook.go
+++ pkg/webhook/clusterresourceoverride/clusterresourceoverride_validating_webhook.go
@@ -43,22 +43,22 @@ func Add(mgr manager.Manager) error {
 // Handle clusterResourceOverrideValidator checks to see if cluster resource override is valid
 func (v *clusterResourceOverrideValidator) Handle(ctx context.Context, req admission.Request) admission.Response {
 	var cro fleetv1alpha1.ClusterResourceOverride
-	klog.V(2).InfoS("Validating webhook handling cluster resource override", "operation", req.Operation)
+	klog.V(2).InfoS("validating webhook handling cluster resource override", "operation", req.Operation)
 	if err := v.decoder.Decode(req, &cro); err != nil {
-		klog.ErrorS(err, "Failed to decode cluster resource override object for validating fields", "userName", req.UserInfo.Username, "groups", req.UserInfo.Groups)
+		klog.ErrorS(err, "failed to decode cluster resource override object for validating fields", "userName", req.UserInfo.Username, "groups", req.UserInfo.Groups)
 		return admission.Errored(http.StatusBadRequest, err)
 	}
 
 	// List of cluster resource overrides
-	croList, err := listClusterResourceOverrideList(ctx, v.client)
+	croList, err := listClusterResourceOverride(ctx, v.client)
 	if err != nil {
 		return admission.Errored(http.StatusBadRequest, err)
 	}
 
 	// Check if the override count limit has been reached, if there are at most 100 cluster resource overrides
 	if req.Operation == admissionv1.Create && len(croList.Items) >= 100 {
-		klog.Errorf("clusterResourceOverride limit has been reached. At most 100 cluster resources can be created.")
-		return admission.Denied("clusterResourceOverride limit has been reached. At most 100 cluster resources can be created.")
+		klog.Errorf("clusterResourceOverride limit has been reached: at most 100 cluster resources can be created.")
+		return admission.Denied("clusterResourceOverride limit has been reached: at most 100 cluster resources can be created.")
 	}
 
 	if err := validator.ValidateClusterResourceOverride(cro, croList); err != nil {
@@ -68,11 +68,11 @@ func (v *clusterResourceOverrideValidator) Handle(ctx context.Context, req admis
 	return admission.Allowed("clusterResourceOverride has valid fields")
 }
 
-// listClusterResourceOverrideList returns a list of cluster resource overrides.
-func listClusterResourceOverrideList(ctx context.Context, client client.Client) (*fleetv1alpha1.ClusterResourceOverrideList, error) {
+// listClusterResourceOverride returns a list of cluster resource overrides.
+func listClusterResourceOverride(ctx context.Context, client client.Client) (*fleetv1alpha1.ClusterResourceOverrideList, error) {
 	croList := &fleetv1alpha1.ClusterResourceOverrideList{}
 	if err := client.List(ctx, croList); err != nil {
-		klog.ErrorS(err, "Failed to list clusterResourceOverrides when validating")
+		klog.ErrorS(err, "failed to list clusterResourceOverrides when validating")
 		return nil, fmt.Errorf("failed to list clusterResourceOverrides, please retry the request: %w", err)
 	}
 	return croList, nil
