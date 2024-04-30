c7cbafa37d1c15bcc770c14acc9753e5e3325d79
Zhiying Lin <54013513+zhiying-lin@users.noreply.github.com>
2024-04-30 11:05:27 +0800

    fix: remove duplicate import (#790)

--- test/e2e/placement_cro_test.go
+++ test/e2e/placement_cro_test.go
@@ -12,8 +12,6 @@ import (
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-
 	placementv1alpha1 "go.goms.io/fleet/apis/placement/v1alpha1"
 	placementv1beta1 "go.goms.io/fleet/apis/placement/v1beta1"
 )
