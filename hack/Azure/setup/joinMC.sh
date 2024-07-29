3915f0099ade7a57b726453028d3c27ef8db068e
Arvind Thirumurugan <arvindthirumurugan@gmail.com>
2024-07-29 14:14:52 -0700

    minor changes to joinMC.sh script

--- hack/Azure/setup/joinMC.sh
+++ hack/Azure/setup/joinMC.sh
@@ -1,11 +1,13 @@
-# CAN ONLY BE RUN AFTER CREATING NEEDED AKS CLUSTERS AND HUB CLUSTER. This script creates member clusters from
-# AKS Cluster's and joins them onto the hub cluster.
+# CAN ONLY BE RUN AFTER CREATING NEEDED AKS CLUSTERS AND HUB CLUSTER. This script creates member clusters for
+# AKS Clusters and joins them onto the fleet hub cluster.
 
-export HUB_CLUSTER="$1"
+export AGENT_IMAGE_VERSION="$1"
+
+export HUB_CLUSTER="$2"
 export HUB_CLUSTER_CONTEXT=$(kubectl config view -o jsonpath="{.contexts[?(@.context.cluster==\"$HUB_CLUSTER\")].name}")
 export HUB_CLUSTER_ADDRESS=$(kubectl config view -o jsonpath="{.clusters[?(@.name==\"$HUB_CLUSTER\")].cluster.server}")
 
-for MC in "${@:2}"; do
+for MC in "${@:3}"; do
 
 # Note that Fleet will recognize your cluster with this name once it joins.
 export MEMBER_CLUSTER=$(kubectl config view -o jsonpath="{.contexts[?(@.context.cluster==\"$MC\")].name}")
@@ -16,16 +18,16 @@ export SERVICE_ACCOUNT="$MEMBER_CLUSTER-hub-cluster-access"
 #echo "Switching into hub cluster context..."
 kubectl config use-context $HUB_CLUSTER_CONTEXT
 
-echo "Create test namespace"
-kubectl create namespace test-ns
+echo "Create namespace to host resources required to connect to fleet"
+kubectl create namespace connect-to-fleet
 
 # The service account can, in theory, be created in any namespace; for simplicity reasons,
-# here you will use the namespace reserved by Fleet installation, `fleet-system`.
+# we create our own namespace `connect-to-fleet` to host the service account and the secret.
 #
 # Note that if you choose a different value, commands in some steps below need to be
 # modified accordingly.
 echo "Creating member service account..."
-kubectl create serviceaccount $SERVICE_ACCOUNT -n test-ns
+kubectl create serviceaccount $SERVICE_ACCOUNT -n connect-to-fleet
 
 echo "Creating member service account secret..."
 export SERVICE_ACCOUNT_SECRET="$MEMBER_CLUSTER-hub-cluster-access-token"
@@ -34,14 +36,14 @@ apiVersion: v1
 kind: Secret
 metadata:
     name: $SERVICE_ACCOUNT_SECRET
-    namespace: test-ns
+    namespace: connect-to-fleet
     annotations:
         kubernetes.io/service-account.name: $SERVICE_ACCOUNT
 type: kubernetes.io/service-account-token
 EOF
 
 echo "Creating member cluster CR..."
-export TOKEN="$(kubectl get secret $SERVICE_ACCOUNT_SECRET -n test-ns -o jsonpath='{.data.token}' | base64 --decode)"
+export TOKEN="$(kubectl get secret $SERVICE_ACCOUNT_SECRET -n connect-to-fleet -o jsonpath='{.data.token}' | base64 --decode)"
 cat <<EOF | kubectl apply -f -
 apiVersion: cluster.kubernetes-fleet.io/v1beta1
 kind: MemberCluster
@@ -51,7 +53,7 @@ spec:
     identity:
         name: $MEMBER_CLUSTER-hub-cluster-access
         kind: ServiceAccount
-        namespace: test-ns
+        namespace: connect-to-fleet
         apiGroup: ""
     heartbeatPeriodSeconds: 15
 EOF
@@ -65,7 +67,6 @@ EOF
 # more information.
 echo "Retrieving image..."
 export REGISTRY="mcr.microsoft.com/aks/fleet"
-export FLEET_VERSION="v0.10.8"
 export MEMBER_AGENT_IMAGE="member-agent"
 export REFRESH_TOKEN_IMAGE="${REFRESH_TOKEN_NAME:-refresh-token}"
 export OUTPUT_TYPE="${OUTPUT_TYPE:-type=docker}"
@@ -85,9 +86,9 @@ echo "Installing member-agent..."
 helm install member-agent charts/member-agent/ \
         --set config.hubURL=$HUB_CLUSTER_ADDRESS  \
         --set image.repository=$REGISTRY/$MEMBER_AGENT_IMAGE \
-        --set image.tag=$FLEET_VERSION \
+        --set image.tag=$AGENT_IMAGE_VERSION \
         --set refreshtoken.repository=$REGISTRY/$REFRESH_TOKEN_IMAGE \
-        --set refreshtoken.tag=$FLEET_VERSION \
+        --set refreshtoken.tag=$AGENT_IMAGE_VERSION \
         --set image.pullPolicy=Always \
         --set refreshtoken.pullPolicy=Always \
         --set config.memberClusterName=$MEMBER_CLUSTER \
