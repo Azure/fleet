f36f2a46b8693a5b7492bdf1d97fd837ef55489e
Jamison Tsai <jamisontsai@microsoft.com>
2024-11-15 15:25:43 +1100

    fix k8serrors capital

--- test/e2e/resource_validation_test.go
+++ test/e2e/resource_validation_test.go
@@ -38,7 +38,7 @@ var _ = Describe("Resource validation tests for Member Cluster", func() {
 		By(fmt.Sprintf("expecting denial of CREATE API %s", name))
 		err := hubClient.Create(ctx, memberClusterName)
 		var statusErr *k8serrors.StatusError
-		Expect(errors.As(err, &statusErr)).To(BeTrue(), fmt.Sprintf("Create API call produced error %s. Error type wanted is %s.", reflect.TypeOf(err), reflect.TypeOf(&k8sErrors.StatusError{})))
+		Expect(errors.As(err, &statusErr)).To(BeTrue(), fmt.Sprintf("Create API call produced error %s. Error type wanted is %s.", reflect.TypeOf(err), reflect.TypeOf(&k8serrors.StatusError{})))
 		Expect(statusErr.Status().Message).Should(ContainSubstring("metadata.name max length is 63"))
 	})
 
@@ -61,7 +61,7 @@ var _ = Describe("Resource validation tests for Member Cluster", func() {
 		By(fmt.Sprintf("expecting denial of CREATE API %s", name))
 		err := hubClient.Create(ctx, memberClusterName)
 		var statusErr *k8serrors.StatusError
-		Expect(errors.As(err, &statusErr)).To(BeTrue(), fmt.Sprintf("Create API call produced error %s. Error type wanted is %s.", reflect.TypeOf(err), reflect.TypeOf(&k8sErrors.StatusError{})))
+		Expect(errors.As(err, &statusErr)).To(BeTrue(), fmt.Sprintf("Create API call produced error %s. Error type wanted is %s.", reflect.TypeOf(err), reflect.TypeOf(&k8serrors.StatusError{})))
 		Expect(statusErr.Status().Message).Should(ContainSubstring("a lowercase RFC 1123 subdomain must consist of lower case alphanumeric characters, '-' or '.', and must start and end with an alphanumeric character (e.g. 'example.com', regex used for validation is '[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*')"))
 	})
 
@@ -84,7 +84,7 @@ var _ = Describe("Resource validation tests for Member Cluster", func() {
 		By(fmt.Sprintf("expecting denial of CREATE API %s", name))
 		err := hubClient.Create(ctx, memberClusterName)
 		var statusErr *k8serrors.StatusError
-		Expect(errors.As(err, &statusErr)).To(BeTrue(), fmt.Sprintf("Create API call produced error %s. Error type wanted is %s.", reflect.TypeOf(err), reflect.TypeOf(&k8sErrors.StatusError{})))
+		Expect(errors.As(err, &statusErr)).To(BeTrue(), fmt.Sprintf("Create API call produced error %s. Error type wanted is %s.", reflect.TypeOf(err), reflect.TypeOf(&k8serrors.StatusError{})))
 		Expect(statusErr.Status().Message).Should(ContainSubstring("a lowercase RFC 1123 subdomain must consist of lower case alphanumeric characters, '-' or '.', and must start and end with an alphanumeric character (e.g. 'example.com', regex used for validation is '[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*')"))
 	})
 
@@ -107,7 +107,7 @@ var _ = Describe("Resource validation tests for Member Cluster", func() {
 		By(fmt.Sprintf("expecting denial of CREATE API %s", name))
 		err := hubClient.Create(ctx, memberClusterName)
 		var statusErr *k8serrors.StatusError
-		Expect(errors.As(err, &statusErr)).To(BeTrue(), fmt.Sprintf("Create API call produced error %s. Error type wanted is %s.", reflect.TypeOf(err), reflect.TypeOf(&k8sErrors.StatusError{})))
+		Expect(errors.As(err, &statusErr)).To(BeTrue(), fmt.Sprintf("Create API call produced error %s. Error type wanted is %s.", reflect.TypeOf(err), reflect.TypeOf(&k8serrors.StatusError{})))
 		Expect(statusErr.Status().Message).Should(ContainSubstring("a lowercase RFC 1123 subdomain must consist of lower case alphanumeric characters, '-' or '.', and must start and end with an alphanumeric character (e.g. 'example.com', regex used for validation is '[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*')"))
 	})
 })
