4c89154dbaecf2f60e0429f5af65b50954f1158e
Jamison Tsai <jamisontsai@microsoft.com>
2024-11-15 15:17:21 +1100

    add regex e2e tests

--- test/e2e/resource_validation_test.go
+++ test/e2e/resource_validation_test.go
@@ -41,4 +41,73 @@ var _ = Describe("Resource validation tests for Member Cluster", func() {
 		Expect(errors.As(err, &statusErr)).To(BeTrue(), fmt.Sprintf("Create API call produced error %s. Error type wanted is %s.", reflect.TypeOf(err), reflect.TypeOf(&k8sErrors.StatusError{})))
 		Expect(statusErr.Status().Message).Should(ContainSubstring("metadata.name max length is 63"))
 	})
+
+	It("should deny creating API with invalid name starting with non-alphanumeric character", func() {
+		var name = "-abcdef-123456789-123456789-123456789-123456789-123456789-123456789-123456789"
+		// Create the API.
+		memberClusterName := &clusterv1beta1.MemberCluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: name,
+			},
+			Spec: clusterv1beta1.MemberClusterSpec{
+				Identity: rbacv1.Subject{
+					Name:      "fleet-member-agent-cluster-1",
+					Kind:      "ServiceAccount",
+					Namespace: "fleet-system",
+					APIGroup:  "",
+				},
+			},
+		}
+		By(fmt.Sprintf("expecting denial of CREATE API %s", name))
+		err := hubClient.Create(ctx, memberClusterName)
+		var statusErr *k8serrors.StatusError
+		Expect(errors.As(err, &statusErr)).To(BeTrue(), fmt.Sprintf("Create API call produced error %s. Error type wanted is %s.", reflect.TypeOf(err), reflect.TypeOf(&k8sErrors.StatusError{})))
+		Expect(statusErr.Status().Message).Should(ContainSubstring("a lowercase RFC 1123 subdomain must consist of lower case alphanumeric characters, '-' or '.', and must start and end with an alphanumeric character (e.g. 'example.com', regex used for validation is '[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*')"))
+	})
+
+	It("should deny creating API with invalid name ending with non-alphanumeric character", func() {
+		var name = "abcdef-123456789-123456789-123456789-123456789-123456789-123456789-123456789-"
+		// Create the API.
+		memberClusterName := &clusterv1beta1.MemberCluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: name,
+			},
+			Spec: clusterv1beta1.MemberClusterSpec{
+				Identity: rbacv1.Subject{
+					Name:      "fleet-member-agent-cluster-1",
+					Kind:      "ServiceAccount",
+					Namespace: "fleet-system",
+					APIGroup:  "",
+				},
+			},
+		}
+		By(fmt.Sprintf("expecting denial of CREATE API %s", name))
+		err := hubClient.Create(ctx, memberClusterName)
+		var statusErr *k8serrors.StatusError
+		Expect(errors.As(err, &statusErr)).To(BeTrue(), fmt.Sprintf("Create API call produced error %s. Error type wanted is %s.", reflect.TypeOf(err), reflect.TypeOf(&k8sErrors.StatusError{})))
+		Expect(statusErr.Status().Message).Should(ContainSubstring("a lowercase RFC 1123 subdomain must consist of lower case alphanumeric characters, '-' or '.', and must start and end with an alphanumeric character (e.g. 'example.com', regex used for validation is '[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*')"))
+	})
+
+	It("should deny creating API with invalid name containing character that is not alphanumeric and not -", func() {
+		var name = "a_bcdef-123456789-123456789-123456789-123456789-123456789-123456789-123456789"
+		// Create the API.
+		memberClusterName := &clusterv1beta1.MemberCluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: name,
+			},
+			Spec: clusterv1beta1.MemberClusterSpec{
+				Identity: rbacv1.Subject{
+					Name:      "fleet-member-agent-cluster-1",
+					Kind:      "ServiceAccount",
+					Namespace: "fleet-system",
+					APIGroup:  "",
+				},
+			},
+		}
+		By(fmt.Sprintf("expecting denial of CREATE API %s", name))
+		err := hubClient.Create(ctx, memberClusterName)
+		var statusErr *k8serrors.StatusError
+		Expect(errors.As(err, &statusErr)).To(BeTrue(), fmt.Sprintf("Create API call produced error %s. Error type wanted is %s.", reflect.TypeOf(err), reflect.TypeOf(&k8sErrors.StatusError{})))
+		Expect(statusErr.Status().Message).Should(ContainSubstring("a lowercase RFC 1123 subdomain must consist of lower case alphanumeric characters, '-' or '.', and must start and end with an alphanumeric character (e.g. 'example.com', regex used for validation is '[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*')"))
+	})
 })
