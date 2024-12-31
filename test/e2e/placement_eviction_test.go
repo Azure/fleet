8cfae772be84ef40d1786b2d93acf31c21f3070a
Arvind Thirumurugan <arvindth@microsoft.com>
2024-12-31 13:44:29 -0800

    address comments

--- test/e2e/placement_eviction_test.go
+++ test/e2e/placement_eviction_test.go
@@ -148,7 +148,7 @@ var _ = Describe("ClusterResourcePlacement eviction of bound binding, no taint s
 	It("should still place resources on the all available member clusters", checkIfPlacedWorkResourcesOnAllMemberClusters)
 })
 
-var _ = Describe("ClusterResourcePlacement eviction of bound binding - PickAll CRP, PDB specified, eviction denied", Ordered, Serial, func() {
+var _ = Describe("ClusterResourcePlacement eviction of bound binding - PickAll CRP, PDB specified to protect resources on all clusters, eviction denied", Ordered, func() {
 	crpName := fmt.Sprintf(crpNameTemplate, GinkgoParallelProcess())
 	crpEvictionName := fmt.Sprintf(crpEvictionNameTemplate, GinkgoParallelProcess())
 
@@ -181,7 +181,7 @@ var _ = Describe("ClusterResourcePlacement eviction of bound binding - PickAll C
 			Spec: placementv1alpha1.PlacementDisruptionBudgetSpec{
 				MinAvailable: &intstr.IntOrString{
 					Type:   intstr.Int,
-					IntVal: 3,
+					IntVal: int32(len(allMemberClusterNames)),
 				},
 			},
 		}
@@ -217,7 +217,7 @@ var _ = Describe("ClusterResourcePlacement eviction of bound binding - PickAll C
 	It("should still place resources on the all available member clusters", checkIfPlacedWorkResourcesOnAllMemberClusters)
 })
 
-var _ = Describe("ClusterResourcePlacement eviction of bound binding - PickAll CRP, PDB specified, eviction allowed", Ordered, Serial, func() {
+var _ = Describe("ClusterResourcePlacement eviction of bound binding - PickAll CRP, PDB specified to protect resources in all but one cluster, eviction allowed", Ordered, Serial, func() {
 	crpName := fmt.Sprintf(crpNameTemplate, GinkgoParallelProcess())
 	crpEvictionName := fmt.Sprintf(crpEvictionNameTemplate, GinkgoParallelProcess())
 	taintClusterNames := []string{memberCluster1EastProdName}
@@ -252,7 +252,7 @@ var _ = Describe("ClusterResourcePlacement eviction of bound binding - PickAll C
 			Spec: placementv1alpha1.PlacementDisruptionBudgetSpec{
 				MinAvailable: &intstr.IntOrString{
 					Type:   intstr.Int,
-					IntVal: 2,
+					IntVal: int32(len(allMemberClusterNames)) - 1,
 				},
 			},
 		}
@@ -306,7 +306,7 @@ var _ = Describe("ClusterResourcePlacement eviction of bound binding - PickAll C
 	})
 })
 
-var _ = Describe("ClusterResourcePlacement eviction of bound binding - PickN CRP, PDB with MaxUnavailable specified as Integer, eviction blocked", Ordered, Serial, func() {
+var _ = Describe("ClusterResourcePlacement eviction of bound binding - PickN CRP, PDB with MaxUnavailable specified to protect resources on all clusters, eviction denied", Ordered, func() {
 	crpName := fmt.Sprintf(crpNameTemplate, GinkgoParallelProcess())
 	crpEvictionName := fmt.Sprintf(crpEvictionNameTemplate, GinkgoParallelProcess())
 
@@ -325,7 +325,7 @@ var _ = Describe("ClusterResourcePlacement eviction of bound binding - PickN CRP
 			Spec: placementv1beta1.ClusterResourcePlacementSpec{
 				Policy: &placementv1beta1.PlacementPolicy{
 					PlacementType:    placementv1beta1.PickNPlacementType,
-					NumberOfClusters: ptr.To(int32(3)),
+					NumberOfClusters: ptr.To(int32(len(allMemberClusterNames))),
 				},
 				ResourceSelectors: workResourceSelector(),
 			},
@@ -390,7 +390,7 @@ var _ = Describe("ClusterResourcePlacement eviction of bound binding - PickN CRP
 	It("should still place resources on the all available member clusters", checkIfPlacedWorkResourcesOnAllMemberClusters)
 })
 
-var _ = Describe("ClusterResourcePlacement eviction of bound binding - PickN CRP, PDB with MaxUnavailable specified as percentage, eviction allowed", Ordered, Serial, func() {
+var _ = Describe("ClusterResourcePlacement eviction of bound binding - PickN CRP, PDB with MaxUnavailable specified as percentage to protect all clusters but one, eviction allowed", Ordered, Serial, func() {
 	crpName := fmt.Sprintf(crpNameTemplate, GinkgoParallelProcess())
 	crpEvictionName := fmt.Sprintf(crpEvictionNameTemplate, GinkgoParallelProcess())
 	taintClusterNames := []string{memberCluster1EastProdName}
@@ -410,7 +410,7 @@ var _ = Describe("ClusterResourcePlacement eviction of bound binding - PickN CRP
 			Spec: placementv1beta1.ClusterResourcePlacementSpec{
 				Policy: &placementv1beta1.PlacementPolicy{
 					PlacementType:    placementv1beta1.PickNPlacementType,
-					NumberOfClusters: ptr.To(int32(3)),
+					NumberOfClusters: ptr.To(int32(len(allMemberClusterNames))),
 				},
 				ResourceSelectors: workResourceSelector(),
 			},
@@ -492,4 +492,96 @@ var _ = Describe("ClusterResourcePlacement eviction of bound binding - PickN CRP
 			Eventually(resourcePlacedActual, eventuallyDuration, eventuallyInterval).Should(Succeed(), "Failed to place resources on the selected clusters")
 		}
 	})
+})
+
+var _ = FDescribe("ClusterResourcePlacement eviction of bound binding - PickN CRP, PDB with MaxUnavailable specified as percentage to protect resources on all clusters, eviction denied", Ordered, func() {
+	crpName := fmt.Sprintf(crpNameTemplate, GinkgoParallelProcess())
+	crpEvictionName := fmt.Sprintf(crpEvictionNameTemplate, GinkgoParallelProcess())
+
+	BeforeAll(func() {
+		By("creating work resources")
+		createWorkResources()
+
+		// Create the CRP.
+		crp := &placementv1beta1.ClusterResourcePlacement{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: crpName,
+				// Add a custom finalizer; this would allow us to better observe
+				// the behavior of the controllers.
+				Finalizers: []string{customDeletionBlockerFinalizer},
+			},
+			Spec: placementv1beta1.ClusterResourcePlacementSpec{
+				Policy: &placementv1beta1.PlacementPolicy{
+					PlacementType:    placementv1beta1.PickNPlacementType,
+					NumberOfClusters: ptr.To(int32(len(allMemberClusterNames))),
+				},
+				ResourceSelectors: workResourceSelector(),
+			},
+		}
+		Expect(hubClient.Create(ctx, crp)).To(Succeed(), "Failed to create CRP %s", crpName)
+	})
+
+	AfterAll(func() {
+		ensureCRPEvictionDeletion(crpEvictionName)
+		ensureCRPDisruptionBudgetDeletion(crpName)
+		ensureCRPAndRelatedResourcesDeletion(crpName, allMemberClusters)
+	})
+
+	It("should update cluster resource placement status as expected", func() {
+		crpStatusUpdatedActual := crpStatusUpdatedActual(workResourceIdentifiers(), allMemberClusterNames, nil, "0")
+		Eventually(crpStatusUpdatedActual, eventuallyDuration, eventuallyInterval).Should(Succeed(), "Failed to update cluster resource placement status as expected")
+	})
+
+	It("should place resources on the all available member clusters", checkIfPlacedWorkResourcesOnAllMemberClusters)
+
+	It("create cluster resource placement disruption budget to block eviction", func() {
+		crpdb := placementv1alpha1.ClusterResourcePlacementDisruptionBudget{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: crpName,
+			},
+			Spec: placementv1alpha1.PlacementDisruptionBudgetSpec{
+				MaxUnavailable: &intstr.IntOrString{
+					Type:   intstr.String,
+					StrVal: "0%",
+				},
+			},
+		}
+		Expect(hubClient.Create(ctx, &crpdb)).To(Succeed(), "Failed to create CRP Disruption Budget %s", crpName)
+	})
+
+	It("create cluster resource placement eviction targeting member cluster 1", func() {
+		crpe := &placementv1alpha1.ClusterResourcePlacementEviction{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: crpEvictionName,
+			},
+			Spec: placementv1alpha1.PlacementEvictionSpec{
+				PlacementName: crpName,
+				ClusterName:   memberCluster1EastProdName,
+			},
+		}
+		Expect(hubClient.Create(ctx, crpe)).To(Succeed(), "Failed to create CRP eviction %s", crpe.Name)
+	})
+
+	It("should update cluster resource placement eviction status as expected", func() {
+		crpEvictionStatusUpdatedActual := testutilseviction.StatusUpdatedActual(
+			ctx, hubClient, crpEvictionName,
+			&testutilseviction.IsValidEviction{IsValid: true, Msg: condition.EvictionValidMessage},
+			&testutilseviction.IsExecutedEviction{IsExecuted: false, Msg: fmt.Sprintf(condition.EvictionBlockedPDBSpecifiedMessageFmt, 3, 3)})
+		Eventually(crpEvictionStatusUpdatedActual, eventuallyDuration, eventuallyInterval).Should(Succeed(), "Failed to update cluster resource placement eviction status as expected")
+	})
+
+	It("should update cluster resource placement eviction status as expected", func() {
+		crpEvictionStatusUpdatedActual := testutilseviction.StatusUpdatedActual(
+			ctx, hubClient, crpEvictionName,
+			&testutilseviction.IsValidEviction{IsValid: true, Msg: condition.EvictionValidMessage},
+			&testutilseviction.IsExecutedEviction{IsExecuted: false, Msg: fmt.Sprintf(condition.EvictionBlockedPDBSpecifiedMessageFmt, 3, 3)})
+		Eventually(crpEvictionStatusUpdatedActual, eventuallyDuration, eventuallyInterval).Should(Succeed(), "Failed to update cluster resource placement eviction status as expected")
+	})
+
+	It("should ensure cluster resource placement status is unchanged", func() {
+		crpStatusUpdatedActual := crpStatusUpdatedActual(workResourceIdentifiers(), allMemberClusterNames, nil, "0")
+		Eventually(crpStatusUpdatedActual, eventuallyDuration, eventuallyInterval).Should(Succeed(), "Failed to update cluster resource placement status as expected")
+	})
+
+	It("should still place resources on the all available member clusters", checkIfPlacedWorkResourcesOnAllMemberClusters)
 })
