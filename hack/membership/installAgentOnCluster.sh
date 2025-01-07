a2c38528a20a905dd319f93d6161ad8f43c770b9
Jamison Tsai <jamisontsai@microsoft.com>
2025-01-07 11:49:31 +1100

    Install the hub agent helm chart on the hub cluster

--- hack/membership/installAgentOnCluster.sh
+++ hack/membership/installAgentOnCluster.sh
@@ -142,4 +142,55 @@ helm install member-agent charts/member-agent/ \
 kubectl get pods -A
 kubectl config use-context $HUB_CLUSTER_CONTEXT
 kubectl get membercluster $MEMBER_CLUSTER
+
+echo "Installing hub-agent..."
+# Install the hub agent helm chart on the hub cluster
+helm install hub-agent charts/hub-agent/ \
+  --set image.pullPolicy=Always \
+  --set image.repository=$REGISTRY/hub-agent \
+  --set image.tag=$TAG \
+  --set logVerbosity=5 \
+  --set namespace=fleet-system \
+  --set enableWebhook=false \
+  --set webhookClientConnectionType=service \
+  --set enableV1Alpha1APIs=false \
+  --set enableV1Beta1APIs=true \
+  --set clusterUnhealthyThreshold="3m0s" \
+  --set forceDeleteWaitTime="1m0s" \
+  --set resources.limits.cpu=4 \
+  --set resources.limits.memory=4Gi \
+  --set concurrentClusterPlacementSyncs=10 \
+  --set ConcurrentRolloutSyncs=20 \
+  --set hubAPIQPS=100 \
+  --set hubAPIBurst=1000 \
+  --set logFileMaxSize=100000000 \
+  --set MaxFleetSizeSupported=100
+
+# Check the status of the hub agent
+kubectl get pods -n fleet-system
+
+echo "Installing prometheus endpoint..."
+# Update prometheus and grafana to the hub cluster
+helm repo update
+
+# Install prometheus fleet metrics
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Service
+metadata:
+  name: fleet-prometheus-endpoint
+  namespace: fleet-system
+spec:
+  selector:
+    app.kubernetes.io/name: hub-agent
+  ports:
+    - protocol: TCP
+      port: 8080
+      targetPort: 8080
+  type: LoadBalancer
+EOF
+
+# Check the status of the service
+kubectl get service -n fleet-system
+
 done
