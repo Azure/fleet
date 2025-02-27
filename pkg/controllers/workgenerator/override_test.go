3f995dd9748d0f8d7c88ed0d96a085e1d1597d1f
Zhiying Lin <zhiyingl456@gmail.com>
2025-02-27 14:54:03 +0800

    address comments

--- pkg/controllers/workgenerator/override_test.go
+++ pkg/controllers/workgenerator/override_test.go
@@ -2115,7 +2115,7 @@ func TestApplyJSONPatchOverride(t *testing.T) {
 			},
 		},
 		{
-			name: "reset the labels",
+			name: "reset the labels using add operation",
 			deployment: appsv1.Deployment{
 				TypeMeta: deploymentType,
 				ObjectMeta: metav1.ObjectMeta{
@@ -2146,6 +2146,37 @@ func TestApplyJSONPatchOverride(t *testing.T) {
 			},
 		},
 		{
+			name: "reset the labels using replace operation",
+			deployment: appsv1.Deployment{
+				TypeMeta: deploymentType,
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "deployment-name",
+					Namespace: "deployment-namespace",
+					Labels: map[string]string{
+						"app": "nginx-1",
+						"key": "value",
+					},
+				},
+			},
+			overrides: []placementv1alpha1.JSONPatchOverride{
+				{
+					Operator: placementv1alpha1.JSONPatchOverrideOpReplace,
+					Path:     "/metadata/labels",
+					Value:    apiextensionsv1.JSON{Raw: []byte(`{"app": "nginx"}`)},
+				},
+			},
+			wantDeployment: appsv1.Deployment{
+				TypeMeta: deploymentType,
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "deployment-name",
+					Namespace: "deployment-namespace",
+					Labels: map[string]string{
+						"app": "nginx",
+					},
+				},
+			},
+		},
+		{
 			name: "add the first label key value",
 			deployment: appsv1.Deployment{
 				TypeMeta: deploymentType,
@@ -2156,6 +2187,7 @@ func TestApplyJSONPatchOverride(t *testing.T) {
 			},
 			overrides: []placementv1alpha1.JSONPatchOverride{
 				{
+					// To add the first key, it cannot use "replace" as the path is missing.
 					Operator: placementv1alpha1.JSONPatchOverrideOpAdd,
 					Path:     "/metadata/labels",
 					Value:    apiextensionsv1.JSON{Raw: []byte(`{"app": "nginx"}`)},
