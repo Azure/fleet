12272778a33ffb082cee025685da46ea5e8da0ef
Arvind Thirumurugan <arvindth@microsoft.com>
2025-02-28 16:29:31 -0800

    add retry with backoff

--- tools/cordon-cluster/main/main.go
+++ tools/cordon-cluster/main/main.go
@@ -6,11 +6,12 @@ import (
 	"log"
 	"os"
 
-	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
+	k8errors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/retry"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	clusterv1beta1 "go.goms.io/fleet/apis/cluster/v1beta1"
@@ -45,7 +46,7 @@ func main() {
 	clusterConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
 		&clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeconfigPath},
 		&clientcmd.ConfigOverrides{
-			CurrentContext: "kind-hub",
+			CurrentContext: *hubClusterContext,
 		})
 
 	restConfig, err := clusterConfig.ClientConfig()
@@ -55,7 +56,7 @@ func main() {
 
 	hubClient, err := client.New(restConfig, client.Options{Scheme: scheme})
 	if err != nil {
-		log.Fatalf("failed to hub cluster client")
+		log.Fatalf("failed to create hub cluster client")
 	}
 
 	var memberClusterList clusterv1beta1.MemberClusterList
@@ -94,52 +95,71 @@ func main() {
 		log.Fatalf("failed to find any CRP which has propagated resource to cluster %s", *clusterName)
 	}
 
-	// retry until update succeeds or non conflict error occurs.
-	for {
-		err := updateMemberClusterWithTaint(ctx, hubClient, *clusterName)
-		if err == nil || !k8sErrors.IsConflict(err) {
-			break
+	err = retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		var mc clusterv1beta1.MemberCluster
+		if err := hubClient.Get(ctx, types.NamespacedName{Name: *clusterName}, &mc); err != nil {
+			return err
 		}
+		cordonTaint := clusterv1beta1.Taint{
+			Key:    "cordon-key",
+			Value:  "cordon-value",
+			Effect: "NoSchedule",
+		}
+
+		// search to see cordonTaint already exists on the cluster.
+		for i := range mc.Spec.Taints {
+			if mc.Spec.Taints[i].Key == cordonTaint.Key {
+				return nil
+			}
+		}
+
+		// add taint to member cluster to cordon.
+		mc.Spec.Taints = append(mc.Spec.Taints, cordonTaint)
+
+		return hubClient.Update(ctx, &mc)
+	})
+
+	if err != nil {
+		log.Fatalf("failed to update member cluster with taint: %v", err)
 	}
 
 	// create eviction objects for all <crpName, targetCluster>.
 	for crpName := range crpNameMap {
-		// TODO: check to see if eviction object already exists, delete it.
-		eviction := placementv1beta1.ClusterResourcePlacementEviction{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: "test-eviction" + crpName,
-			},
-			Spec: placementv1beta1.PlacementEvictionSpec{
-				PlacementName: crpName,
-				ClusterName:   *clusterName,
-			},
+		evictionName := "test-eviction" + crpName
+
+		if err = removeExistingEviction(ctx, hubClient, evictionName); err != nil {
+			log.Fatalf("failed to remove existing eviction for CRP %s", crpName)
 		}
-		if err := hubClient.Create(ctx, &eviction); err != nil {
-			log.Fatalf("failed to create CRP eviction for CRP %s", crpName)
+
+		err = retry.OnError(retry.DefaultBackoff, func(err error) bool {
+			return k8errors.IsAlreadyExists(err)
+		}, func() error {
+			eviction := placementv1beta1.ClusterResourcePlacementEviction{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: evictionName,
+				},
+				Spec: placementv1beta1.PlacementEvictionSpec{
+					PlacementName: crpName,
+					ClusterName:   *clusterName,
+				},
+			}
+			return hubClient.Create(ctx, &eviction)
+		})
+
+		if err != nil {
+			log.Fatalf("failed to create eviction for CRP %s: %v", crpName, err)
 		}
 	}
 }
 
-func updateMemberClusterWithTaint(ctx context.Context, hubClient client.Client, mcName string) error {
-	var mc clusterv1beta1.MemberCluster
-	if err := hubClient.Get(ctx, types.NamespacedName{Name: mcName}, &mc); err != nil {
-		return err
-	}
-	cordonTaint := clusterv1beta1.Taint{
-		Key:    "cordon-key",
-		Value:  "cordon-value",
-		Effect: "NoSchedule",
-	}
-
-	// search to see cordonTaint already exists on the cluster.
-	for i := range mc.Spec.Taints {
-		if mc.Spec.Taints[i].Key == cordonTaint.Key {
+func removeExistingEviction(ctx context.Context, client client.Client, evictionName string) error {
+	eviction := placementv1beta1.ClusterResourcePlacementEviction{}
+	if err := client.Get(ctx, types.NamespacedName{Name: evictionName}, &eviction); err != nil {
+		if k8errors.IsNotFound(err) {
 			return nil
 		}
+		return err
 	}
 
-	// add taint to member cluster to cordon.
-	mc.Spec.Taints = append(mc.Spec.Taints, cordonTaint)
-
-	return hubClient.Update(ctx, &mc)
+	return client.Delete(ctx, &eviction)
 }
