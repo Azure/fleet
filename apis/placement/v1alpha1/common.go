150b05cb648d213f2804cf503f01c0ee5a5f0398
Ryan Zhang <yangzhangrice@hotmail.com>
2025-03-12 16:11:03 -0700

    feat: support arbitrary key on the cluster as the value of override (#1074)

--- apis/placement/v1alpha1/common.go
+++ apis/placement/v1alpha1/common.go
@@ -17,4 +17,13 @@ const (
 
 	// OverrideClusterNameVariable is the reserved variable in the override value that will be replaced by the actual cluster name.
 	OverrideClusterNameVariable = "${MEMBER-CLUSTER-NAME}"
+
+	// OverrideClusterLabelKeyVariablePrefix is a reserved variable in the override expression.
+	// We use this variable to find the associated the key following the prefix.
+	// The key name ends with a "}" character (but not include it).
+	// The key name must be a valid Kubernetes label name and case-sensitive.
+	// The content of the string containing this variable will be replaced by the actual label value on the member cluster.
+	// For example, if the string is "${MEMBER-CLUSTER-LABEL-KEY-kube-fleet.io/region}" then the key name is "kube-fleet.io/region".
+	// If there is a label "kube-fleet.io/region": "us-west-1" on the member cluster, this string will be replaced by "us-west-1".
+	OverrideClusterLabelKeyVariablePrefix = "${MEMBER-CLUSTER-LABEL-KEY-"
 )
