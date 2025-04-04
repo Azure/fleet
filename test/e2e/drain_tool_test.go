0d5ea437c0827b74971aeb5f2b449806a532732f
Arvind Thirumurugan <arvindth@microsoft.com>
2025-04-04 12:05:14 -0700

    add tests

---
+++ test/e2e/drain_tool_test.go
@@ -0,0 +1,112 @@
+/*
+Copyright (c) Microsoft Corporation.
+Licensed under the MIT license.
+*/
+
+package e2e
+
+import (
+	"fmt"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	placementv1beta1 "go.goms.io/fleet/apis/placement/v1beta1"
+	"go.goms.io/fleet/test/e2e/framework"
+	"go.goms.io/fleet/tools/draincluster/drain"
+	"go.goms.io/fleet/tools/uncordoncluster/uncordon"
+)
+
+var _ = Describe("Drain cluster", Ordered, Serial, func() {
+	crpName := fmt.Sprintf(crpNameTemplate, GinkgoParallelProcess())
+	var d *drain.Helper
+	var u *uncordon.Helper
+	var noDrainClusterNames []string
+	var drainClusters, noDrainClusters []*framework.Cluster
+
+	BeforeAll(func() {
+		drainClusters = []*framework.Cluster{memberCluster1EastProd}
+
+		noDrainClusters = []*framework.Cluster{memberCluster2EastCanary, memberCluster3WestProd}
+		noDrainClusterNames = []string{memberCluster2EastCanaryName, memberCluster3WestProdName}
+
+		By("creating work resources")
+		createWorkResources()
+
+		// Create the CRP.
+		crp := &placementv1beta1.ClusterResourcePlacement{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: crpName,
+				// Add a custom finalizer; this would allow us to better observe
+				// the behavior of the controllers.
+				Finalizers: []string{customDeletionBlockerFinalizer},
+			},
+			Spec: placementv1beta1.ClusterResourcePlacementSpec{
+				Policy: &placementv1beta1.PlacementPolicy{
+					PlacementType: placementv1beta1.PickAllPlacementType,
+				},
+				ResourceSelectors: workResourceSelector(),
+			},
+		}
+		Expect(hubClient.Create(ctx, crp)).To(Succeed(), "Failed to create CRP %s", crpName)
+
+		d = &drain.Helper{
+			HubClient:                            hubClient,
+			ClusterName:                          memberCluster1EastProdName,
+			ClusterResourcePlacementResourcesMap: make(map[string][]placementv1beta1.ResourceIdentifier),
+		}
+
+		u = &uncordon.Helper{
+			HubClient:   hubClient,
+			ClusterName: memberCluster1EastProdName,
+		}
+	})
+
+	AfterAll(func() {
+		ensureCRPAndRelatedResourcesDeleted(crpName, allMemberClusters)
+	})
+
+	It("should update cluster resource placement status as expected", func() {
+		crpStatusUpdatedActual := crpStatusUpdatedActual(workResourceIdentifiers(), allMemberClusterNames, nil, "0")
+		Eventually(crpStatusUpdatedActual, eventuallyDuration, eventuallyInterval).Should(Succeed(), "Failed to update cluster resource placement status as expected")
+	})
+
+	It("should place resources on the all available member clusters", checkIfPlacedWorkResourcesOnAllMemberClusters)
+
+	It("drain cluster", func() {
+		isDrainSuccessful, err := d.Drain(ctx)
+		Expect(isDrainSuccessful).To(BeTrue(), "Failed to drain member cluster")
+		Expect(err).To(Succeed(), "Failed to drain member cluster")
+	})
+
+	It("should ensure no resources exist on drained clusters", func() {
+		for _, cluster := range drainClusters {
+			resourceRemovedActual := workNamespaceRemovedFromClusterActual(cluster)
+			Eventually(resourceRemovedActual, eventuallyDuration, eventuallyInterval).Should(Succeed(), "Failed to check if resources doesn't exist on member cluster")
+		}
+	})
+
+	It("should update cluster resource placement status as expected", func() {
+		crpStatusUpdatedActual := crpStatusUpdatedActual(workResourceIdentifiers(), noDrainClusterNames, nil, "0")
+		Eventually(crpStatusUpdatedActual, eventuallyDuration, eventuallyInterval).Should(Succeed(), "Failed to update cluster resource placement status as expected")
+	})
+
+	It("should place resources on the selected clusters with no taint", func() {
+		for _, cluster := range noDrainClusters {
+			resourcePlacedActual := workNamespaceAndConfigMapPlacedOnClusterActual(cluster)
+			Eventually(resourcePlacedActual, eventuallyDuration, eventuallyInterval).Should(Succeed(), "Failed to place resources on the selected clusters")
+		}
+	})
+
+	It("uncordon cluster", func() {
+		Expect(u.Uncordon(ctx)).To(Succeed(), "Failed to uncordon member cluster")
+	})
+
+	It("should update cluster resource placement status as expected", func() {
+		crpStatusUpdatedActual := crpStatusUpdatedActual(workResourceIdentifiers(), allMemberClusterNames, nil, "0")
+		Eventually(crpStatusUpdatedActual, eventuallyDuration, eventuallyInterval).Should(Succeed(), "Failed to update cluster resource placement status as expected")
+	})
+
+	It("should place resources on the all available member clusters", checkIfPlacedWorkResourcesOnAllMemberClusters)
+})
