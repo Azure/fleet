4d2d00f19c0fb62aed03e8702a55e8a1931d7569
michaelawyu <chenyu1@microsoft.com>
2025-05-02 04:22:50 +1000

    chore: move the envelope CRDs to the v1beta1 API group (#44)

--- apis/placement/v1beta1/envelope_types.go
+++ apis/placement/v1beta1/envelope_types.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package v1alpha1
+package v1beta1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -26,36 +26,21 @@ import (
 // +kubebuilder:object:root=true
 // +kubebuilder:resource:scope="Cluster",categories={fleet,fleet-placement}
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:storageversion
 
 // ClusterResourceEnvelope wraps cluster-scoped resources for placement.
 type ClusterResourceEnvelope struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	// The desired state of ClusterResourceEnvelope.
-	// +kubebuilder:validation:Required
-	Spec EnvelopeSpec `json:"spec"`
-}
-
-// EnvelopeSpec helps wrap resources for placement.
-type EnvelopeSpec struct {
-	// A map of wrapped manifests.
+	// The manifests wrapped in this envelope.
 	//
 	// Each manifest is uniquely identified by a string key, typically a filename that represents
-	// the manifest.
+	// the manifest. The value is the manifest object itself.
 	// +kubebuilder:validation:Required
 	// +kubebuilder:validation:MinProperties=1
 	// +kubebuilder:validation:MaxProperties=50
-	Manifests map[string]Manifest `json:"manifests"`
-}
-
-// Manifest is a wrapped resource.
-type Manifest struct {
-	// The resource data.
-	// +kubebuilder:validation:Required
-	// +kubebuilder:validation:EmbeddedResource
-	// +kubebuilder:pruning:PreserveUnknownFields
-	Data runtime.RawExtension `json:"data"`
+	Data map[string]runtime.RawExtension `json:"data"`
 }
 
 // +genclient
@@ -63,11 +48,19 @@ type Manifest struct {
 // +kubebuilder:object:root=true
 // +kubebuilder:resource:scope="Namespaced",categories={fleet,fleet-placement}
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:storageversion
+
+// ResourceEnvelope wraps namespaced resources for placement.
 type ResourceEnvelope struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	// The desired state of ResourceEnvelope.
+	// The manifests wrapped in this envelope.
+	//
+	// Each manifest is uniquely identified by a string key, typically a filename that represents
+	// the manifest. The value is the manifest object itself.
 	// +kubebuilder:validation:Required
-	Spec EnvelopeSpec `json:"spec"`
+	// +kubebuilder:validation:MinProperties=1
+	// +kubebuilder:validation:MaxProperties=50
+	Data map[string]runtime.RawExtension `json:"data"`
 }
