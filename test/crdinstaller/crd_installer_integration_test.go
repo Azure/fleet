d3bd9ddd94bcb7ba15de66a94b041e63525cd3aa
Arvind Thirumurugan <arvindth@microsoft.com>
2025-06-10 00:54:01 -0700

    add initial IT

--- test/crdinstaller/crd_installer_integration_test.go
+++ test/crdinstaller/crd_installer_integration_test.go
@@ -20,11 +20,25 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/types"
+	
 	cmdCRDInstaller "go.goms.io/fleet/cmd/crdinstaller/utils"
 )
 
 var _ = Describe("Test CRD Installer", func() {
 	It("should install CRDs and create a CRD instance", func() {
 		Expect(cmdCRDInstaller.InstallCRD(ctx, k8sClient, "./original_crds/test.kubernetes-fleet.io_testresources.yaml")).To(Succeed())
+	})
+
+	It("should verify CRD installation", func() {
+		crdName := "testresources.test.kubernetes-fleet.io"
+		crd := &apiextensionsv1.CustomResourceDefinition{}
+		Expect(k8sClient.Get(ctx, types.NamespacedName{Name: crdName}, crd)).NotTo(HaveOccurred(), "CRD %s should be installed", crdName)
+
+		v1alpha1Version := crd.Spec.Versions[0]
+		Expect(v1alpha1Version.Name).Should(Equal("v1alpha1"), "CRD %s should have version v1alpha1", crdName)
+
+		Expect(v1alpha1Version.Schema.OpenAPIV3Schema.Properties["spec"].Properties["properties"].Properties["bar"]).ShouldNot(BeNil(), "CRD %s should have 'bar' property in spec", crdName)
 	})
 })
