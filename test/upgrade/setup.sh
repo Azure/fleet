a8f761fc69bfa761f4f322785e913898a450bf59
Zhiying Lin <zhiyingl456@gmail.com>
2025-07-03 10:52:17 +0800

    fix: disable the new features when running compatibility tests

--- test/upgrade/setup.sh
+++ test/upgrade/setup.sh
@@ -73,7 +73,9 @@ helm install hub-agent charts/hub-agent/ \
     --set webhookClientConnectionType=service \
     --set forceDeleteWaitTime="1m0s" \
     --set clusterUnhealthyThreshold="3m0s" \
-    --set logFileMaxSize=1000000
+    --set logFileMaxSize=1000000 \
+    --set resourceSnapshotCreationMinimumInterval=0m \
+    --set resourceChangesCollectionDuration=0m
 
 # Install the member agent and related components to the member clusters.
 
