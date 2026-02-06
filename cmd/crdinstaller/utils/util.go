e8c9b39c1a020529ffdf1a29dfa4ea2d0456ab2a
Arvind Thirumurugan <arvindth@microsoft.com>
2026-02-05 18:36:09 -0800

    minor refactor
    Signed-off-by: Arvind Thirumurugan <arvindth@microsoft.com>

--- cmd/crdinstaller/utils/util.go
+++ cmd/crdinstaller/utils/util.go
@@ -117,7 +117,8 @@ func CollectCRDs(crdDirectoryPath, mode string, scheme *runtime.Scheme) ([]apiex
 		// Process based on mode.
 		crdFileName := filepath.Base(crdpath)
 
-		if mode == "member" || mode == "arcMember" {
+		switch mode {
+		case "member", "arcMember":
 			if memberCRD[crdFileName] {
 				crd, err := GetCRDFromPath(crdpath, scheme)
 				if err != nil {
@@ -125,25 +126,16 @@ func CollectCRDs(crdDirectoryPath, mode string, scheme *runtime.Scheme) ([]apiex
 				}
 				crdsToInstall = append(crdsToInstall, *crd)
 			}
-			// Skip CRDs that are not in the memberCRD map.
-			return nil
-		}
-
-		crd, err := GetCRDFromPath(crdpath, scheme)
-		if err != nil {
-			return err
-		}
-
-		// For hub mode, only collect CRDs whose group has substring kubernetes-fleet.io.
-		if mode == "hub" {
+		case "hub":
+			crd, err := GetCRDFromPath(crdpath, scheme)
+			if err != nil {
+				return err
+			}
 			// special case for multicluster external CRD in hub cluster.
 			if multiclusterCRD[crdFileName] {
 				crdsToInstall = append(crdsToInstall, *crd)
-				return nil
-			}
-			group := crd.Spec.Group
-			// Check if the group contains "kubernetes-fleet.io" substring.
-			if strings.Contains(group, "kubernetes-fleet.io") && !memberCRD[crdFileName] {
+			} else if strings.Contains(crd.Spec.Group, "kubernetes-fleet.io") && !memberCRD[crdFileName] {
+				// For hub mode, only collect CRDs whose group has substring kubernetes-fleet.io.
 				crdsToInstall = append(crdsToInstall, *crd)
 			}
 		}
